33bc4746013695d4fb8656012489c7c14ac3c4e7
James Kurczodyna <james@finedesigngroup.com>
2014-08-20 17:02:06 -0700

    setup configurable options

--- lib/fae/engine.rb
+++ lib/fae/engine.rb
@@ -6,5 +6,20 @@ module Fae
 
     # include libraries
     require 'simple_form'
+
+    # set config defaults
+    config
+  end
+
+  # configuraable defaults
+  class << self
+    mattr_accessor :title, :logo_path
+    self.title      = "My FINE Admin"
+    self.logo_path  = "/assets/fae/logo.png"
+  end
+
+  # this function maps the vars from your app into your engine
+  def self.setup(&block)
+    yield self
   end
 end
