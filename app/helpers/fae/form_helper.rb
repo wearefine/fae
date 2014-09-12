ac9f9a411118a2addec7d7c329b55f9ea5288051
dannysperry <danny@finedesigngroup.com>
2014-09-12 09:21:21 -0700

    added fae_prefix and fae-suffix helper_methods

--- app/helpers/fae/form_helper.rb
+++ app/helpers/fae/form_helper.rb
@@ -2,38 +2,41 @@ module Fae
   module FormHelper
 
     def fae_input(f, attribute, options={})
-      class_option options
-      label_and_hint_for(attribute, options)
+      custom_options options
+      label_and_hint(attribute, options)
 
       f.input attribute, options
     end
 
 
-    def fae_prefix(f, attribute, options={})
-      if options[:prefix].present?
-        case options[:prefix]
-        when "$"
-          prefix_class = "input-icon-currency"
-        when "%"
-          prefix_class = "input-icon-percentage"
-        else
-          #TODO - we can ignore the fact that their prefix isn't a valid prefix, or we can raise an error
-        end
-        options[:wrapper_html] = { class: prefix_class }
-      end
 
+    def fae_prefix(f, attribute, text,  options={})
+      symbol 'prefix', text, options
+      fae_input f, attribute, options
+    end
+
+    def fae_suffix(f, attribute, text, options={})
+      symbol 'suffix', text, options
       fae_input f, attribute, options
     end
 
     private
 
-    def label_and_hint_for(attribute, options)
+    def label_and_hint(attribute, options)
       options[:label] = "#{ options[:label] || attribute.to_s.titleize }<h6 class='helper_text'>#{options[:helper_text]}</h6>".html_safe if options[:helper_text].present?
       options[:hint] = options[:hint].html_safe if options[:hint].present?
     end
 
-    def class_option options
+    def custom_options options
       options[:input_html] = { class: options[:class] } if options[:class].present?
+    end
+
+    def symbol(type, val, options)
+      options[:as] = :symbol
+      options[:span_class]  = "input-symbol--#{type}"
+      options[:span_class] += " icon-#{val}" if options[:icon].present?
+
+      options[:content_text] = val if options[:icon].blank?
     end
   end
 end
