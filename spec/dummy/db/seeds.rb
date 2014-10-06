4ad455ed0fe63fb5e0608c9241034c483b73f615
dannysperry <danny@finedesigngroup.com>
2014-10-06 13:12:54 -0700

    moved dummy app seeds data

---
+++ spec/dummy/db/seeds.rb
@@ -0,0 +1,13 @@
+(1..4).each do |i|
+
+  Wine.create(name: "wine #{i}")
+
+  Acclaim.create(score: "#{100-(2*1)}", publication: "The #{i.ordinalize} publication for an acclaim", description: "description"*i )
+
+  Varietal.create(name: "varietal #{i}")
+
+  SellingPoint.create(name: "selling point #{i}")
+
+  Person.create(name: "host ##{i}")
+
+end
