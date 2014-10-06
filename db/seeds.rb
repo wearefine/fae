3d582f751cb584a0354557ca001704ccf6633c0d
dannysperry <danny@finedesigngroup.com>
2014-10-06 11:48:56 -0700

    added seed data and refactored home page private methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@ Fae::User.create(
   role: super_admin,
   active: true
   )
+
+(1..4).each do |i|
+
+  Wine.create(name: "wine #{i}")
+
+  Acclaim.create(score: "#{100-(2*1)}", publication: "The #{i.ordinalize} publication for an acclaim", description: "description"*i )
+
+  Varietal.create(name: "varietal #{i}")
+
+  SellingPoint.create(name: "selling point #{i}")
+
+  Person.create(name: "host ##{i}")
+
+end
