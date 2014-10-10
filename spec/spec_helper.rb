21c1ed5694383f10bafeedc3c74121396d11f382
James Kurczodyna <james@finedesigngroup.com>
2014-10-10 15:46:20 -0700

    require yarjuf in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
+require 'yarjuf'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
