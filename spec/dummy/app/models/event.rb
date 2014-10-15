6503d2de25ac2daa88f7b69010414198106768e8
dannysperry <danny@finedesigngroup.com>
2014-10-15 15:53:46 -0700

    james says, "France is not a city"

--- spec/dummy/app/models/event.rb
+++ spec/dummy/app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   include Fae::Concerns::Models::Base
 
-  validates :city, inclusion: ["Los Angeles", "San Francisco", "Portland", "France"];
+  validates :city, inclusion: ["Los Angeles", "San Francisco", "Portland"];
 
   belongs_to :release
   has_many :people
