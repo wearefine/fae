c1bce7919ecee5fc1ec5d1ee4da33f87c07f36c2
dannysperry <danny@finedesigngroup.com>
2014-10-15 15:01:24 -0700

    removed unneeded comment schema

--- spec/dummy/app/models/event.rb
+++ spec/dummy/app/models/event.rb
@@ -1,15 +1,6 @@
 class Event < ActiveRecord::Base
   include Fae::Concerns::Models::Base
 
-    # t.string   "name"
-    # t.date     "start_date"
-    # t.date     "end_date"
-    # t.string   "event_type"
-    # t.string   "city"
-    # t.integer  "person_id"
-    # t.datetime "created_at"
-    # t.datetime "updated_at"
-
   validates :city, inclusion: ["Los Angeles", "San Francisco", "Portland", "France"];
 
   belongs_to :release
