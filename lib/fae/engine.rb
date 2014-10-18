864416a232caf4fac3ed800b0779bb0865db07ef
James Kurczodyna <james@finedesigngroup.com>
2014-10-17 17:00:28 -0700

    require remotipart in engine

--- lib/fae/engine.rb
+++ lib/fae/engine.rb
@@ -8,6 +8,7 @@ module Fae
     # include libraries
     require 'simple_form'
     require 'jquery-ui-rails'
+    require 'remotipart'
 
     config.railties_order = [:main_app, Fae::Engine, :all]
     config.autoload_paths += %W(#{config.root}/lib)
