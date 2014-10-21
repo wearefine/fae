e1ee56e0ac3454c29d09f9671b2ccf36f962198f
dannysperry <danny@finedesigngroup.com>
2014-10-21 09:46:53 -0700

    changed default login from super.admin to admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ super_admin = Fae::Role.create(name: 'super admin', position: 0)
 Fae::User.create(
   first_name: 'FINE',
   last_name: 'admin',
-  email: 'super.admin@finedesigngroup.com',
+  email: 'admin@finedesigngroup.com',
   password: 'doingfine',
   role: super_admin,
   active: true
