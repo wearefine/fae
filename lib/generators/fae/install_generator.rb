6aeeeaf08b5419c9319e8235d87b4f887afa5b55
James Kurczodyna <james@finedesigngroup.com>
2014-10-30 10:36:39 -0700

    generate randon devise key on install

--- lib/generators/fae/install_generator.rb
+++ lib/generators/fae/install_generator.rb
@@ -4,16 +4,24 @@ module Fae
     source_root File.expand_path('../templates', __FILE__)
 
     def install
-      # set_source
-
       run 'bundle install'
       route "mount Fae::Engine => '/admin'"
       # copy templates and generators
       copy_file File.expand_path(File.join(__FILE__, "../templates/tasks/fae_tasks.rake")), "lib/tasks/fae_tasks.rake"
-      # build fae initializer and generate key
+      build_initializer
       rake 'fae:install:migrations'
       rake 'db:migrate'
       rake 'fae:seed_db'
+    end
+
+  private
+
+    def build_initializer
+      copy_file File.expand_path(File.join(__FILE__, "../templates/initializers/fae.rb")), "config/initializers/fae.rb"
+      inject_into_file "config/initializers/fae.rb", after: "Fae.setup do |config|\n" do <<-RUBY
+\n  config.devise_secret_key = '#{SecureRandom.hex(64)}'\n
+RUBY
+      end
     end
 
   end
