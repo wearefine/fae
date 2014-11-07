9088e0955fe43d4baa6e5600ba794898986df13e
James Kurczodyna <james@wearefine.com>
2014-11-06 16:17:28 -0800

    remove unneeded pages associations (will move to SubClasses)

--- app/models/fae/page.rb
+++ app/models/fae/page.rb
@@ -1,21 +1,5 @@
 module Fae
   class Page < ActiveRecord::Base
-    has_many :text_areas,
-             as: :contentable,
-             class_name: 'Fae::TextArea',
-             dependent: :destroy
-    accepts_nested_attributes_for :text_areas, allow_destroy: true
 
-    has_many :files,
-             as: :fileable,
-             class_name: 'Fae::File',
-             dependent: :destroy
-    accepts_nested_attributes_for :files, allow_destroy: true
-
-    has_many :text_fields,
-             as: :contentable,
-             class_name: 'Fae::TextField',
-             dependent: :destroy
-    accepts_nested_attributes_for :text_field, allow_destroy: true
   end
 end
