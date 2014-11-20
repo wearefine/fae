65bf8756bd05e36ae9915be847f9910cca2ee55a
James Kurczodyna <james@wearefine.com>
2014-11-19 16:08:15 -0800

    add login feature specs

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+feature 'Sign In' do
+
+  scenario 'when user leaves out email' do
+    visit fae.new_user_session_path
+
+    fill_in 'user_password', with: 'password'
+    click_button 'Submit'
+
+    expect(page).to have_content('You’ll need a user and a password that works.')
+    expect(page).to_not have_content('Welcome')
+  end
+
+  scenario 'when user leaves out password' do
+    visit fae.new_user_session_path
+
+    fill_in 'user_email', with: 'test@test.com'
+    click_button 'Submit'
+
+    expect(page).to have_content('You’ll need a user and a password that works.')
+    expect(page).to_not have_content('Welcome')
+  end
+
+  scenario "when user doesn't exist" do
+    visit fae.new_user_session_path
+
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Submit'
+
+    expect(page).to have_content('You’ll need a user and a password that works.')
+    expect(page).to_not have_content('Welcome')
+  end
+
+  before :each do
+    role = FactoryGirl.create(:fae_role, name: 'admin')
+    @user = FactoryGirl.create(:fae_user,
+      email: 'test@test.com',
+      password: 'passord1',
+      password_confirmation: 'passord1',
+      active: true
+      )
+  end
+
+  scenario "when user isn't active" do
+    @user.update_attribute(:active, false)
+
+    visit fae.new_user_session_path
+
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'passord1'
+    click_button 'Submit'
+
+    expect(page).to have_content('Your account is not activated yet.')
+    expect(page).to_not have_content('Welcome')
+  end
+
+  scenario "when user is active" do
+    visit fae.new_user_session_path
+
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'passord1'
+    click_button 'Submit'
+
+    expect(page).to have_content('Welcome')
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
