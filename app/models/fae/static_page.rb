ed5f1a2125029b442755f9b7c27898366a827949
dannyatFINE <danny@finedesigngroup.com>
2014-12-04 14:20:01 -0800

    updated fae static pages has_one associations to get called in .instance

--- app/models/fae/static_page.rb
+++ app/models/fae/static_page.rb
@@ -6,6 +6,11 @@ module Fae
       if row.blank?
         row = StaticPage.create(title: @slug.titleize, slug: @slug)
       end
+      # create has_one associations
+      row.fae_fields.each do |key, value|
+        row.class.send :has_one, key.to_sym, -> { where(attached_as: key.to_s)}, as: :contentable, class_name: value.to_s, dependent: :destroy
+        row.class.send :accepts_nested_attributes_for, key, allow_destroy: true
+      end
       row
     end
 
