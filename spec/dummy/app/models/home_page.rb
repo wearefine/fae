fd4acc7d772597588745047412a241586b67de02
James Kurczodyna <james@wearefine.com>
2014-12-05 12:00:11 -0800

    add a bunch more fields for benchmarking

--- spec/dummy/app/models/home_page.rb
+++ spec/dummy/app/models/home_page.rb
@@ -3,11 +3,21 @@ class HomePage < Fae::StaticPage
 
   @slug = 'home'
 
-  # required to set the has_one associations, the static controller will build these associations dynamically
+  # required to set the has_one associations, Fae::StaticPage will build these associations dynamically
   def fae_fields
     {
       hero: Fae::TextField,
       introduction: Fae::TextArea,
+      body_copy_1: Fae::TextArea,
+      body_copy_2: Fae::TextArea,
+      body_copy_3: Fae::TextArea,
+      body_copy_4: Fae::TextArea,
+      body_copy_5: Fae::TextArea,
+      body_copy_6: Fae::TextArea,
+      body_copy_7: Fae::TextArea,
+      body_copy_8: Fae::TextArea,
+      body_copy_9: Fae::TextArea,
+      body_copy_10: Fae::TextArea,
       hero_image: Fae::Image,
       welocme_pdf: Fae::File
     }
