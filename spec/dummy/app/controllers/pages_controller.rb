addaa3075f17418197734a68eaaec75024a348dd
James Kurczodyna <james@wearefine.com>
2014-12-05 15:30:07 -0800

    add HomePage specs

--- spec/dummy/app/controllers/pages_controller.rb
+++ spec/dummy/app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @home_page = HomePage.instance
   end
 end
---
+++ spec/factories/home_page_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :home_page do
+    title 'Home'
+    slug 'home'
+  end
+
+end
