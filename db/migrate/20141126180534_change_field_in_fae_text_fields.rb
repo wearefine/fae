43359507869654b1ea185e6812145dbdf16d1458
James Kurczodyna <james@wearefine.com>
2014-12-17 10:17:43 -0800

    update migrations to fix fea install

--- db/migrate/20141126180534_change_field_in_fae_text_fields.rb
+++ db/migrate/20141126180534_change_field_in_fae_text_fields.rb
@@ -1,5 +1,6 @@
 class ChangeFieldInFaeTextFields < ActiveRecord::Migration
   def change
     rename_column :fae_text_fields, :attatched_as, :attached_as
+    rename_index :fae_text_fields, :attatched_as, :attached_as
   end
 end
