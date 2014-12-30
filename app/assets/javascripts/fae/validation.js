aac26b6e942540b928540fa04241a775cef348c5
dannyatFINE <danny@finedesigngroup.com>
2014-12-29 21:52:39 -0800

    added a check on input fields to make sure the value hasn't changed and updated jslint errors

--- app/assets/javascripts/fae/validation.js
+++ app/assets/javascripts/fae/validation.js
@@ -1,93 +1,120 @@
 var Validator = {
 
-  init: function() {
+  init: function () {
     this.validate();
     this.formValidate();
   },
 
   vars: {
+    IS_VALID: '',
     chosen_select: '.chosen-container .chosen-search input'
   },
 
-  formValidate: function() {
-    var IS_VALID;
-    Validator.attemptSubmit();
+  formValidate: function () {
+    this.attemptSubmit();
   },
 
-  validate: function(){
-    $('[data-validate]').each(function(i) {
-      var validationInput = $(this);
-      if ( validationInput.data('validate').length ) {
-        if( validationInput.siblings('.chosen-container').length ) {
-          validationInput = Validator.isChosen(validationInput);
-          $('body').on('blur', Validator.vars.chosen_select, function() {
+  validate: function () {
+    var that = this;
+    $('[data-validate]').each(function () {
+      var $validationInput = $(this);
+      var originalValue = $validationInput.val();
+      if ($validationInput.data('validate').length) {
+        if ($validationInput.siblings('.chosen-container').length) {
+          $validationInput = that.isChosen($validationInput);
+          $('body').on('propertychange change click keyup input paste', that.vars.chosen_select, function () {
             var $chosenInput = $(this);
-            Validator.judgeIt( $chosenInput );
+            if (originalValue !== $(this).val()) { that.judgeIt($chosenInput); }
           });
         } else {
-          validationInput.on('blur', function(){
-            $input = $(this);
-            Validator.judgeIt( $input );
+          $validationInput.on('propertychange change click keyup input paste', function () {
+            var $input = $(this);
+            if (originalValue !== $(this).val()) { that.judgeIt($input); }
           });
         }
       }
     });
   },
+
+ //  $('.myElements').each(function () {
+ //   var elem = $(this);
+
+ //   // Save current value of element
+ //   elem.data('oldVal', elem.val());
+
+ //   // Look for changes in the value
+ //   elem.bind("propertychange change click keyup input paste", function (event){
+ //      // If value has changed...
+ //      if (elem.data('oldVal') != elem.val()) {
+ //       // Updated stored value
+ //       elem.data('oldVal', elem.val());
+
+ //       // Do action
+ //       ....
+ //     }
+ //   });
+ // });
 
   // private functions
 
-  attemptSubmit: function(){
-    $('form').on('submit',function(e) {
-      IS_VALID = true;
-      $('[data-validate]').each(function(i) {
+  attemptSubmit: function () {
+    var that = this;
+    $('form').on('submit', function (e) {
+      that.vars.IS_VALID = true;
+      $('[data-validate]').each(function () {
         var $validationInput = $(this);
         if ($validationInput.data('validate').length) {
-          Validator.judgeIt($validationInput);
+          that.judgeIt($validationInput);
         }
       });
-      if (IS_VALID === false) {e.preventDefault();}
-    });
-  },
-
-  judgeIt: function($input) {
-    judge.validate($input[0], {
-      valid: function() {
-        Validator.createSuccessClass($input);
-      },
-      invalid: function(input, messages) {
-        IS_VALID = false;
-        Validator.labelNameInMessage($input, messages);
-        Validator.createOrReplaceError($input, messages);
+      if (that.vars.IS_VALID === false) {
+        e.preventDefault();
       }
     });
   },
 
-  isChosen: function(input) {
-    input.siblings( Validator.vars.chosen_select );
+  judgeIt: function ($input) {
+    var that = this;
+    judge.validate($input[0], {
+      valid: function () {
+        that.createSuccessClass($input);
+      },
+      invalid: function (input, messages) {
+        that.vars.IS_VALID = false;
+        that.labelNameInMessage($input, messages);
+        that.createOrReplaceError($input, messages);
+      }
+    });
   },
 
-  createSuccessClass: function($input) {
+  isChosen: function (input) {
+    var that = this;
+    input.siblings(that.vars.chosen_select);
+  },
+
+  createSuccessClass: function ($input) {
     $input.addClass('valid').removeClass('invalid');
     $input.parent().removeClass('field_with_errors').children('.error').remove();
   },
 
-  labelNameInMessage: function(elm, messages){
-    for (var i = messages.length - 1; i >= 0; i--) {
-      var siblings = elm.siblings('label');
-      if (siblings.get(0).childNodes[0].nodeName == "ABBR") {var index = 1};
-      var index = index || 0;
+  labelNameInMessage: function (elm, messages) {
+    var i, siblings, index;
+    for (i = messages.length - 1; i >= 0; i--) {
+      siblings = elm.siblings('label');
+      if (siblings.get(0).childNodes[0].nodeName === "ABBR") { index = 1;}
+      index = index || 0;
       messages[i] = siblings.get(0).childNodes[index].nodeValue + " " + messages[i];
-    };
+    }
   },
 
-  createOrReplaceError: function($input, messages) {
+  createOrReplaceError: function ($input, messages) {
     $input.addClass('invalid').removeClass('valid');
     if ($input.parent('.input').children('.error').length) {
       $input.parent('.input').children('.error').text(messages.join(','));
     } else {
-      $input.parent('.input').addClass('field_with_errors').append("<span class='error'>"+messages.join(',')+"</span>");
-    };
+      $input.parent('.input').addClass('field_with_errors').append("<span class='error'>" + messages.join(',') + "</span>");
+    }
   }
 
 
-}
+};
