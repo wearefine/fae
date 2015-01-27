4e6c763e5a97384009bc071dede0b3507565b9fd
James Kurczodyna <james@wearefine.com>
2015-01-26 16:35:19 -0800

    dummy clean, notes to document

--- spec/dummy/app/models/home_page.rb
+++ spec/dummy/app/models/home_page.rb
@@ -7,16 +7,7 @@ class HomePage < Fae::StaticPage
     {
       hero: Fae::TextField,
       introduction: Fae::TextArea,
-      body_copy_1: Fae::TextArea,
-      body_copy_2: Fae::TextArea,
-      body_copy_3: Fae::TextArea,
-      body_copy_4: Fae::TextArea,
-      body_copy_5: Fae::TextArea,
-      body_copy_6: Fae::TextArea,
-      body_copy_7: Fae::TextArea,
-      body_copy_8: Fae::TextArea,
-      body_copy_9: Fae::TextArea,
-      body_copy_10: Fae::TextArea,
+      body: Fae::TextArea,
       hero_image: Fae::Image,
       welcome_pdf: Fae::File
     }
--- spec/dummy/config/initializers/fae.rb
+++ spec/dummy/config/initializers/fae.rb
@@ -4,28 +4,31 @@ Fae.setup do |config|
 
   config.nav_items = [
     {
-      text: "Releases", path: "/admin/releases"
+      text: 'Releases', path: '/admin/releases'
     },
     {
-      text: "Wines", path: "/admin/wines"
+      text: 'Wines', path: '/admin/wines'
     },
     {
-      text: "Acclaim", path: "/admin/acclaims"
+      text: 'Acclaim', path: '/admin/acclaims'
     },
     {
-      text: "Varietal", path: "/admin/varietals"
+      text: 'Varietal', path: '/admin/varietals'
     },
     {
-      text: "Selling Point", path: "/admin/selling_points"
+      text: 'Selling Point', path: '/admin/selling_points'
     },
     {
-      text: "Event", path: "/admin/events"
+      text: 'Event', path: '/admin/events'
     },
     {
-      text: "Event Hosts", path: "/admin/people"
+      text: 'Event Hosts', path: '/admin/people'
     },
     {
-      text: "Locations", path: "/admin/locations"
+      text: 'Locations', path: '/admin/locations'
+    },
+    {
+      text: 'Pages', path: '/admin/pages'
     }
   ]
 end
