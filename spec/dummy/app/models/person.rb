a2a74211808a3b54a51a456ada444b201c7c6a55
dannyatFINE <danny@finedesigngroup.com>
2015-02-11 12:58:29 -0800

    44817 - added presence validation on Person#name

--- spec/dummy/app/models/person.rb
+++ spec/dummy/app/models/person.rb
@@ -8,5 +8,7 @@ class Person < ActiveRecord::Base
   has_one :image, as: :imageable, class_name: '::Fae::Image', dependent: :destroy
   accepts_nested_attributes_for :image, allow_destroy: true
 
+  validates_presence_of :name
+
   belongs_to :event
 end
