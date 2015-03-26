ac075318f097342a702df6a9733766edfb28c431
James Kurczodyna <james@wearefine.com>
2015-03-26 13:24:25 -0700

    refs #44609 - cancel btn on settings form should go to dashboard

--- spec/requests/fae/users_spec.rb
+++ spec/requests/fae/users_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe 'users#index' do
 
-  let(:edit_user) { FactoryGirl.create(:fae_user) }
-
   context 'when role is super admin' do
     before do
       super_admin_login
@@ -11,12 +9,6 @@ describe 'users#index' do
 
     it 'should be able to access users' do
       get fae.users_path
-
-      expect(response.status).to eq(200)
-    end
-
-    it 'should be able to access user edit page' do
-      get fae.edit_user_path(edit_user)
 
       expect(response.status).to eq(200)
     end
@@ -29,12 +21,6 @@ describe 'users#index' do
 
     it 'should be able to access users' do
       get fae.users_path
-
-      expect(response.status).to eq(200)
-    end
-
-    it 'should be able to access user edit page' do
-      get fae.edit_user_path(edit_user)
 
       expect(response.status).to eq(200)
     end
@@ -51,6 +37,58 @@ describe 'users#index' do
       expect(response.status).to eq(302)
       expect(response).to redirect_to(fae.root_path)
     end
+  end
+
+  context 'when logged out' do
+    it "shouldn't be able to access users" do
+      create_super_user
+      get fae.users_path
+
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to(fae.new_user_session_path)
+    end
+  end
+
+end
+
+describe 'users#edit' do
+
+  let(:edit_user) { FactoryGirl.create(:fae_user) }
+
+  context 'when role is super admin' do
+    before do
+      super_admin_login
+    end
+
+    it 'should be able to access user edit page' do
+      get fae.edit_user_path(edit_user)
+
+      expect(response.status).to eq(200)
+    end
+
+    it "should cancel to users#index" do
+      get fae.edit_user_path(edit_user)
+
+      expect(response.body).to include('href="/admin/users" id="main_content-header-save-cancel"')
+    end
+  end
+
+  context 'when role is admin' do
+    before do
+      admin_login
+    end
+
+    it 'should be able to access user edit page' do
+      get fae.edit_user_path(edit_user)
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context 'when role is user' do
+    before do
+      user_login
+    end
 
     it "shouldn't be able to access users" do
       get fae.edit_user_path(edit_user)
@@ -60,10 +98,38 @@ describe 'users#index' do
     end
   end
 
+end
+
+describe 'users#settings' do
+
+  context 'when role is super admin' do
+    before do
+      super_admin_login
+    end
+
+    it "should cancel to dashboard" do
+      get fae.settings_path
+
+      expect(response.body).to include('href="/admin/" id="main_content-header-save-cancel"')
+    end
+  end
+
+  context 'when role is user' do
+    before do
+      user_login
+    end
+
+    it "should cancel to dashboard" do
+      get fae.settings_path
+
+      expect(response.body).to include('href="/admin/" id="main_content-header-save-cancel"')
+    end
+  end
+
   context 'when logged out' do
-    it "shouldn't be able to access users" do
+    it "shouldn't be able to access settings" do
       create_super_user
-      get fae.users_path
+      get fae.settings_path
 
       expect(response.status).to eq(302)
       expect(response).to redirect_to(fae.new_user_session_path)
