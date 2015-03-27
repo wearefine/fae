73c7573c3f7322443562bab4ea0d03cb7d92894f
James Kurczodyna <james@wearefine.com>
2015-03-27 13:37:18 -0700

    refs #44624 - update config to remove deprecation warnings

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -18,6 +18,8 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
 
