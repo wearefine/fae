a138272aff4530bf22b6e0cf5c73c8656699bf98
Niles McGiver <niles@finedesigngroup.com>
2015-04-10 09:24:08 -0700

    removed validation from acclain, and put it on release

--- spec/dummy/app/models/acclaim.rb
+++ spec/dummy/app/models/acclaim.rb
@@ -1,8 +1,6 @@
 class Acclaim < ActiveRecord::Base
   include Fae::Concerns::Models::Base
 
-  validates :publication_date, presence: true
-
   has_one :pdf,
           as: :fileable,
           class_name: '::Fae::File',
