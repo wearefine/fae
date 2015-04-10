9141677188d24ad4af70ec9d2889deb9edea5b0c
Niles McGiver <niles@finedesigngroup.com>
2015-04-10 09:51:37 -0700

    added in method to count text fields

--- app/assets/javascripts/fae/validation.js
+++ app/assets/javascripts/fae/validation.js
@@ -199,5 +199,22 @@ var Validator = {
     }
   }
 
+  counter: function(){
+    var text_max = 99;
+    $('#textarea_feedback').html(text_max + ' characters remaining');
+
+    $('#textarea').keyup(function() {
+        var text_length = $('#textarea').val().length;
+        var text_remaining = text_max - text_length;
+
+        $('#textarea_feedback').html(text_remaining + ' characters remaining');
+    });
+
+
+
+
+
+  }
+
 
 };
