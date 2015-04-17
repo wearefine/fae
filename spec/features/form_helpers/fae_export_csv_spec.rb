690460624374b6d64de5ba6d046ae5e08d0f5a43
Niles McGiver <niles@finedesigngroup.com>
2015-04-17 11:19:56 -0700

    tested an field that won't be on index page to make sure we are looking at the csv output

--- spec/features/form_helpers/fae_export_csv_spec.rb
+++ spec/features/form_helpers/fae_export_csv_spec.rb
@@ -12,14 +12,14 @@ feature 'fae_export_csv' do
 
 
   scenario 'if items present export to csv button should be present', js: true do
-    release1 = FactoryGirl.create(:release, name: "Unique Release", id: 1, release_date: Date.today)
+    release1 = FactoryGirl.create(:release, name: "Unique Release", id: 1, release_date: Date.today, intro: "This is the introduction")
     admin_login
     visit admin_releases_path
 
     expect(page).to have_css('a.csv-button')
     expect(page).to have_content("Export to CSV")
     page.find('.csv-button').click
-    expect(page).to have_content('Unique Release')
+    expect(page).to have_content('This is the introduction')
   end
 
 end
