b72e045c0d8ba11f94c31cc27cc9d5810cdc506d
James Kurczodyna <james@wearefine.com>
2015-07-31 15:37:07 -0700

    adds config.recreate_versions option to Fae initializer

--- app/models/fae/image.rb
+++ app/models/fae/image.rb
@@ -9,6 +9,8 @@ module Fae
     attr_accessor :redirect
     mount_uploader :asset, Fae::ImageUploader
 
+    after_save :recreate_versions
+
     validates :asset,
       file_size: {
         maximum: Fae.max_image_upload_size.megabytes.to_i
@@ -16,48 +18,10 @@ module Fae
 
     belongs_to :imageable, polymorphic: true, touch: true
 
-  private
+    private
 
-    # def crop_image
-    #   asset.recreate_versions! if crop_x_changed? or crop_y_changed? or crop_x2_changed? or crop_y2_changed?
-    # end
-
-    class << self
-
-      # all image info is set here, yes this will get big, but it's best to keep this stuff in one place!
-      def sizing_info
-        info = {
-          hero_image: {
-            min_width: 1500,
-            max_width: 1500,
-            min_height: 0,
-            max_height: 0,
-            notes: 'Any special notations go here. All image info is defined in one place: the image model'
-          },
-          large_ad: {
-            min_width: 720,
-            max_width: 720,
-            min_height: 0,
-            max_height: 0,
-            notes: nil
-          },
-          small_ad: {
-            min_width: 340,
-            max_width: 340,
-            min_height: 0,
-            max_height: 0,
-            notes: nil
-          },
-          gallery_image: {
-            min_width: 1100,
-            max_width: 1100,
-            min_height: 0,
-            max_height: 0,
-            notes: nil
-          }
-        }
-      end
-
+    def recreate_versions
+      asset.recreate_versions! if Fae.recreate_versions && asset.present?
     end
 
   end
