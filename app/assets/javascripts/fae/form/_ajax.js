8c5bdc7613ca049973394b32f733d414d3fb9ab7
Tim Shedor <tim@wearefine.com>
2015-09-23 16:02:15 -0700

    refs #50794 - refactor ajax file

--- app/assets/javascripts/fae/form/_ajax.js
+++ app/assets/javascripts/fae/form/_ajax.js
@@ -4,87 +4,74 @@
 
 Fae.form.ajax = {
 
+  $addedit_form: $('.js-addedit-form, .js-index-addedit-form'),
+  $filter_form: $('.js-filter-form'),
+  has_reinit: false,
+
   init: function() {
-    this.set_elements();
     if(!this.has_reinit) {
-      this.nested_addedit_links();
-      this.nested_addedit_submission();
-      this.index_addedit_links();
-      this.index_addedit_submission();
+      this.addEditLinks();
+      this.addEditSubmission();
     }
-    this.delete_no_form();
-    this.apply_cookies();
+    this.deleteNoForm();
+    this.applyCookies();
     if (this.$filter_form.length) {
-      this.filter_select();
-      this.filter_submission();
+      this.filterSelect();
+      this.filterSubmission();
     }
-    this.image_delete_links();
+    this.imageDeleteLinks();
     this.htmlListeners();
   },
 
-  has_reinit: false,
+  // Click event listener for add and edit links
+  // Applies to both index and nested form
+  addEditLinks: function() {
+    var _this = this;
 
-  set_elements: function() {
-    this.$addedit_form = $('.js-addedit-form');
-    this.$index_addedit_form = $('.js-index-addedit-form');
-    this.$filter_form = $('.js-filter-form');
-  },
-
-  nested_addedit_links: function() {
     this.$addedit_form.on('click', '.js-add-link, .js-edit-link', function(ev) {
       ev.preventDefault();
       var $this = $(this);
-      var $parent = $this.closest('.js-addedit-form');
+      var $parent = $this.hasClass('js-index-add-link') ? $('.js-addedit-form') : $this.closest('.js-addedit-form');
 
       // scroll to the last column of the tbody, where the form will start
-      FCH.smoothScroll($parent.find("tbody tr:last-child"), 500, 100, 90);
+      FCH.smoothScroll($parent.find('tbody tr:last-child'), 500, 100, 90);
 
-      Fae.form.ajax.addedit_actions($this, $parent);
+      _this._addEditActions($this, $parent);
     });
   },
 
-  index_addedit_links: function() {
-    this.$index_addedit_form.on('click', '.js-add-link, .js-edit-link', function(ev) {
-      ev.preventDefault();
-      var $this = $(this);
-      var $parent = $('.js-addedit-form');
-
-      // scroll to the last column of the tbody, where the form will start
-      FCH.smoothScroll($parent.find("tbody tr:last-child"), 500, 100, 90);
-
-      Fae.form.ajax.addedit_actions($this, $parent);
-    });
-  },
-
-  addedit_actions: function(field, $parent) {
+  _addEditActions: function(field, $parent) {
     var $this = $(field);
     var $wrapper = $parent.find('.js-addedit-form-wrapper');
 
     $.get($this.attr('href'), function(data){
       // check to see if the content is hidden and slide it down if it is.
-      if ($wrapper.is(":hidden")) {
+      if( $wrapper.is(':hidden') ) {
         // replace the content of the form area and initiate the chosen and fileinputer
-        $wrapper.html(data).find(".select select").fae_chosen({ width: '300px' });
-        $wrapper.find(".input.file").fileinputer();
+        $wrapper.html(data).find('.select select').fae_chosen({ width: '300px' });
+        $wrapper.find('.input.file').fileinputer();
         $wrapper.slideDown();
+
       } else {
         // if it is visible, replace its content by retaining height
         $wrapper.height($wrapper.height());
 
         // replace the content of the form area and then remove that height and then chosen and then fileinputer
-        $wrapper.html(data).css("height", "").find(".select select").fae_chosen();
-        $wrapper.find(".input.file").fileinputer();
+        $wrapper.html(data).css('height', '').find('.select select').fae_chosen();
+        $wrapper.find('.input.file').fileinputer();
       }
 
+      // Reinitialize form elements
       Fae.form.dates.date_picker();
       Fae.form.dates.daterange_picker();
       Fae.form.text.slugger();
 
-      $wrapper.find(".hint").hinter();
+      $wrapper.find('.hint').hinter();
     });
   },
 
-  nested_addedit_submission: function() {
+  // Once form is submitted and receives a successful AJAX response, replace form data and initialize listeners on nested elements
+  addEditSubmission: function() {
     var _this = this;
 
     this.$addedit_form.on('ajax:success', function(evt, data, status, xhr){
@@ -97,43 +84,32 @@ Fae.form.ajax = {
         var $parent = $this.parent();
         // we're manipulating the return so let's store in a var and keep 'data' intact
         var html = data;
+        var $html = $(html);
 
         // remotipart returns data inside textarea, let's grab it from there
-        if ($(html)[0].localName === 'textarea') {
-          html = $(data)[0].value;
+        if ( $html.is('textarea') ) {
+          html = $data.val();
+          $html = $(html);
         }
 
-        if ($(html)[1] && $(html)[1].className === 'main_content-section-area') {
+        if( $html && ($html.hasClass('main_content-section-area') || $html.hasClass('js-index-addedit-form')) ) {
           // we're returning the table, replace everything
+          _this._addEditReplaceAndReinit($this, $html.innerHTML, $target);
 
-          var $form_wrapper = $this.find('.js-addedit-form-wrapper');
-
-          // if there's a form wrap, slide it up before replacing content
-          if ($form_wrapper.length) {
-            $form_wrapper.slideUp(function(){
-              _this.addedit_replace_and_reinit($this, $(html)[1].innerHTML, $target);
-            });
-          } else {
-            _this.addedit_replace_and_reinit($this, $(html)[1].innerHTML, $target);
-          }
-
-          if (!$target.hasClass("js-delete-link")) {
-            FCH.smoothScroll($parent, 500, 100, 120);
-          }
-        } else if ($(html)[0].className === 'form_content-wrapper') {
+        } else if( $html.hasClass('form_content-wrapper') ) {
           // we're returning the form due to an error, just replace the form
           $this.find('.form_content-wrapper').replaceWith(html);
           $this.find('.select select').fae_chosen();
-          $this.find(".input.file").fileinputer();
+          $this.find('.input.file').fileinputer();
 
           FCH.smoothScroll($this.find('.js-addedit-form-wrapper'), 500, 100, 120);
         }
 
         _this.has_reinit = true;
         _this.init();
-        Fae.fade_notices();
+        Fae.navigation.fade_notices();
 
-      } else if ($target.hasClass("js-asset-delete-link")) {
+      } else if ($target.hasClass('js-asset-delete-link')) {
         // handle remove asset links
         $target.parent().fadeOut('fast', function() {
           $(this).next('.asset-inputs').fadeIn('fast');
@@ -142,65 +118,32 @@ Fae.form.ajax = {
     });
   },
 
-  index_addedit_submission: function() {
-    var _this = this;
+  _addEditReplaceAndReinit: function($el, html, $target) {
+    var $form_wrapper = $el.find('.js-addedit-form-wrapper');
 
-    this.$index_addedit_form.on('ajax:success', function(evt, data, status, xhr){
+    // if there's a form wrap, slide it up before replacing content
+    if ($form_wrapper.length) {
+      $form_wrapper.slideUp(function(){
+        $el.html(html).find('.select select').fae_chosen();
+        Fae.tables.rowSorting();
+      });
 
-      var $target = $(evt.target);
+    } else {
+      $el.html(html).find('.select select').fae_chosen();
+      Fae.tables.rowSorting();
 
-      // ignore calls not returning html
-      if (data !== ' ' && $(data)[0]) {
-        var $this = $(this);
-        var $parent = $this.parent();
+    }
 
-        // we're manipulating the return so let's store in a var and keep 'data' intact
-        var html = data;
-
-        if ($(html)[0] && $(html)[0].className === 'js-index-addedit-form') {
-          // we're returning the table, replace everything
-
-          var $form_wrapper = $this.find('.js-addedit-form-wrapper');
-
-          // if there's a form wrap, slide it up before replacing content
-          if ($form_wrapper.length) {
-            $form_wrapper.slideUp(function(){
-              _this.addedit_replace_and_reinit($this, $(html)[0].innerHTML, $target);
-            });
-          } else {
-            _this.addedit_replace_and_reinit($this, $(html)[0].innerHTML, $target);
-          }
-
-          if (!$target.hasClass("js-delete-link")) {
-            FCH.smoothScroll($parent, 500, 100, 120);
-          }
-        }
-
-        _this.has_reinit = true;
-        _this.init();
-        Fae.fade_notices();
-
-      } else if ($target.hasClass("js-asset-delete-link")) {
-        // handle remove asset links
-        $target.parent().fadeOut('fast', function() {
-          $(this).next('.asset-inputs').fadeIn('fast');
-        });
-      }
-    });
+    if( !$target.hasClass('js-delete-link') ) {
+      FCH.smoothScroll($el.parent(), 500, 100, 120);
+    }
   },
 
-  addedit_replace_and_reinit: function($this, html, $target) {
-    $this.html(html)
-      .find(".select select").fae_chosen();
-
-    Fae.tables.rowSorting();
-  },
-
-  filter_submission: function(params) {
+  filterSubmission: function(params) {
     var _this = this;
     _this.$filter_form
       .on('ajax:success', function(evt, data, status, xhr){
-        $(this).next('table').replaceWith($(data).find('table').first());
+        $(this).next('table').replaceWith( $(data).find('table').first() );
       })
       .on('click', '.js-reset-btn', function(ev) {
         var form = $(this).closest('form')[0];
@@ -214,8 +157,7 @@ Fae.form.ajax = {
       });
   },
 
-  apply_cookies: function() {
-    var _this = this;
+  applyCookies: function() {
     var cookie_key = $('.js-filter-form').data('cookie-key');
 
     if (cookie_key) {
@@ -237,22 +179,15 @@ Fae.form.ajax = {
         }
       }
 
-      var callback = function(params){
-        $.cookie(cookie_key, JSON.stringify(params));
-
-        var hash = window.location.hash;
-        Fae.form.ajax.set_filter_dropdowns(hash);
-      }
-
-      _this.grind = new Grinder(callback);
+      this.grind = new Grinder(this._setFilterDropdowns);
     }
   },
 
   // update hash when filter dropdowns changed
-  filter_select: function(){
+  filterSelect: function(){
     var _this = this;
     $('.js-filter-form .table-filter-group').on('change', function(){
-      if ($('.js-filter-form').data('cookie-key')) {
+      if( $('.js-filter-form').data('cookie-key') ) {
         var key = $(this).find('select').attr('id').split('filter_')[1];
         var value = $(this).find('option:selected').val();
 
@@ -262,55 +197,57 @@ Fae.form.ajax = {
   },
 
   // check for cookie or hash and set dropdowns/ url accordingly
-  set_filter_dropdowns: function(hash) {
-    var cookie_name = $('.js-filter-form').data('cookie-key')
-    if (cookie_name) {
-      var parsed = JSON.parse($.cookie(cookie_name));
-    } else {
-      var parsed = this.grind.parse(hash);
-    }
+  // callback for Grinder
+  _setFilterDropdowns: function(params) {
+    var cookie_name = $('.js-filter-form').data('cookie-key');
+    $.cookie(cookie_name, JSON.stringify(params));
 
-    if (!$.isEmptyObject(parsed)) {
-      $.each(parsed, function(k, v){
+    if( !$.isEmptyObject(params) ) {
+      $.each(params, function(k, v) {
         $('.js-filter-form .table-filter-group').each(function(){
-          var key = $(this).find('select').attr('id').split('filter_')[1];
-          var value = $(this).find('option:selected').val();
-          if (k == key) {
-            $(this).find('option').each(function(){
-              if ($(this).val() == v) {
-                $(this).prop('selected', 'selected');
+          var $this = $(this);
+          var key = $this.find('select').attr('id').split('filter_')[1];
+          var value = $this.find('option:selected').val();
+
+          if( k === key ) {
+            $this.find('option').each(function(){
+              if( $this.val() === v ) {
+                $this.prop('selected', 'selected');
                 $('#filter_' + key).trigger('chosen:updated');
               };
             });
           }
         });
       });
+
       $('.js-filter-form').submit();
     }
   },
 
-  delete_no_form: function() {
+  deleteNoForm: function() {
     // on deletes that don't exist in a form like file upload area
     $('.js-asset-delete-link').on('ajax:success', function(){
       var $this = $(this);
-      if (!$this.closest('.js-addedit-form-wrapper').length) {
+      if( !$this.closest('.js-addedit-form-wrapper').length ) {
         var $parent = $this.closest('.asset-actions');
-        var $inputs = $parent.next('.asset-inputs');
+
         $parent.fadeOut(function(){
-          $inputs.fadeIn();
+          $parent.next('.asset-inputs').fadeIn();
         });
       }
     });
   },
 
   //ajax image delete links
-  image_delete_links: function() {
+  imageDeleteLinks: function() {
     $('.imageDeleteLink').click(function(e) {
       e.preventDefault();
-      if (confirm('Are you sure you want to delete this image?')) {
-        $.post($(this).attr('href'),'html');
-        $(this).parent().next().show();
-        $(this).parent().hide();
+      var $this = $(this);
+
+      if( confirm('Are you sure you want to delete this image?') ) {
+        $.post($this.attr('href'), 'html');
+        $this.parent().next().show();
+        $this.parent().hide();
       }
     });
   },
@@ -322,17 +259,12 @@ Fae.form.ajax = {
       // for the yes/no slider
       .on('click', '.slider-wrapper', function(e){
         e.preventDefault();
-        $(this).toggleClass("slider-yes-selected");
+        $(this).toggleClass('slider-yes-selected');
       })
 
-      // The settings menu for tables
-      .on('click', '.main_table-action_menu-trigger', function(e){
-        $(this).toggleClass("js-active");
-      })
-
-      // for checkboxes
-      .on('click', '.boolean label, .checkbox_collection--vertical label, .checkbox_collection--horizontal label', function(e){
-        $(this).toggleClass("js-active");
+      // The settings menu for tables and checkboxe
+      .on('click', '.main_table-action_menu-trigger, .boolean label, .checkbox_collection--vertical label, .checkbox_collection--horizontal label', function(e){
+        $(this).toggleClass('js-active');
       })
 
       // stop the event bubbling and running the above toggleClass twice
