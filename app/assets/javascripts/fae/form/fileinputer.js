ba6d13336072f7571fbd387e8bc57845c9ae675c
Tim Shedor <tim@wearefine.com>
2015-09-28 17:29:59 -0700

    refs #50794 - refactor the jquery plugins

--- app/assets/javascripts/fae/form/fileinputer.js
+++ app/assets/javascripts/fae/form/fileinputer.js
@@ -1,104 +1,176 @@
-// plugin have a checkbox in the thead of a table that controls that table column's checkboxes.
-var FileInputer = {
-	options: {
-		wrapper_class: "file_input-wrapper",
-		button_class: "file_input-button",
-		delete_class: "file_input-delete",
-		text: "Choose File",
-		file_text: "No File Chosen",
-		active_class: "js-active"
-	},
+'use strict';
 
-	init: function(options, elm){
-		//reference for callback
-		var that = this;
+(function ( $ ) {
 
-		// unite the default options with the passed-in ones
-		this.options = $.extend({delete_class: "icon-delete_x file_input-delete"}, this.options, options);
+  /**
+   * Private initialization of FileInputer object.
+   * @access private
+   * @class
+   */
+  function FileInputer($el, options) {
+    return this.init($el, options);
+  }
 
-		// saving element reference
-		this.elm = elm;
-		this.$input = $(elm).find("input[type=file]");
+  FileInputer.prototype = {
+    /** Inherited settings from jQuery initialization */
+    options: {},
+    /** @type {jQuery} */
+    $el: '',
 
-		// create the visuals
-		this.make_visuals();
+    init: function($el, options) {
+      this.options = options;
+      this.$el = $el;
+      this._drawElements();
+      this.changeListener();
+    },
 
-		// make the events
-		this.clicker();
-		this.onchanger();
-		this.deleter();
-	},
+    /**
+     * Create jQuery objects of all necessary components and add them to the DOM
+     * @access protected
+     * @fires FileInputer._buttonListener
+     * @fires FileInputer._deleterListener
+     */
+    _drawElements: function() {
+      this.$input = this.$el.find('input[type="file"]');
 
-	make_visuals: function() {
-		// positoin the input off screen
-		this.$input.css({
-			"position": "absolute",
-			"top": 0,
-			"left": "-9999px",
-			"visibility": "hidden"
-		});
+      // position the input off screen
+      this.$input.css({
+        position: 'absolute',
+        top: 0,
+        left: '-9999px',
+        visibility: 'hidden'
+      });
 
-		//save the reference for each element to object
-		this.$inputer = $(document.createElement("div")).addClass(this.options.wrapper_class);
-		this.$button = $(document.createElement("button")).addClass(this.options.button_class).attr("type", "button").text(this.options.text);
-		this.$text = $(document.createElement("span")).text(this.options.file_text);
-		this.$deleter = $(document.createElement("div")).addClass(this.options.delete_class);
+      //save the reference for each element to object
+      this.$inputer = $('<div />', {
+        class: this.options.wrapper_class
+      });
 
-		this.$inputer.append(this.$button).append(this.$text).append(this.$deleter);
-		$(this.elm).append(this.$inputer);
-	},
+      this.$text = $('<span />', {
+        text: this.options.file_text
+      });
 
-	clicker: function() {
-		var that = this;
+      var $button = $('<button />', {
+        class: this.options.button_class,
+        attr: {
+          type: 'button'
+        },
+        text: this.options.text
+      });
 
-		// redirect the click on the created visuals to the file input area
-		this.$button.on("click", function(){
-			that.$input.click();
-		});
-	},
+      var $deleter = $('<div />', {
+        class: this.options.delete_class
+      });
 
-	deleter: function() {
-		var that = this;
-		this.$deleter.on("click", function() {
-			that.$input.val("");
-			that.$text.text(that.options.file_text);
-			that.$inputer.removeClass(that.options.active_class);
-		});
-	},
+      this.$inputer
+        .append($button)
+        .append(this.$text)
+        .append($deleter);
 
-	onchanger: function() {
-		var that = this;
+      this.$el.append(this.$inputer);
 
-		// this is to get the filename and present it next to the button
-		this.$input.on("change", function(e){
-			if(that.check_size()) {
-				that.$text.text($(this).val().replace("C:\\fakepath\\", ""));
+      this._deleterListener($deleter);
+      this._buttonListener($button);
+    },
 
-				if ($(this).val() !== "") {
-					that.$inputer.addClass(that.options.active_class);
-				}
-			} else {
-				e.preventDefault();
-			}
-		});
-	},
+    /**
+     * Redirect the click on the created visuals to the file input area
+     * @access protected
+     * @param {jQuery} $button
+     */
+    _buttonListener: function($button) {
+      var _this = this;
 
-	check_size: function() {
-		var limit = parseInt( this.$input.attr('data-limit') );
-		var size = this.$input.get(0).files[0].size / 1024 / 1024;
-		var error_obj = $('<span />', { class: 'error' })
-		error_obj.text( this.$input.attr('data-exceeded').replace('###', limit) )
+      $button.on('click', function(){
+        _this.$input.click();
+      });
+    },
 
-		if(size > limit) {
-			this.$input.after( error_obj );
-			$(this.elm).addClass('field_with_errors');
-			return false;
-		} else {
-			return true;
-		}
+    /**
+     * Clear input, set upload text, and remove active text to destroy appearance of a file uploaded
+     * @access protected
+     * @param {jQuery} $deleter
+     */
+    _deleterListener: function($deleter) {
+      var _this = this;
 
-	}
-};
+      $deleter.on('click', function() {
+        _this.$input.val('');
+        _this.$text.text(_this.options.file_text);
+        _this.$inputer.removeClass(_this.options.active_class);
+      });
+    },
 
-// make it a plugin
-$.plugin("fileinputer", FileInputer);
+    /**
+     * On $input change (a file is uploaded), display file name and set field to active
+     */
+    changeListener: function() {
+      var _this = this;
+
+      // this is to get the filename and present it next to the button
+      _this.$input.on('change', function(e){
+        var $this = $(this);
+
+        if (_this._checkSize()) {
+          _this.$text.text( $this.val().replace('C:\\fakepath\\', '') );
+
+          if ($this.val() !== '') {
+            _this.$inputer.addClass( _this.options.active_class );
+          }
+        } else {
+          e.preventDefault();
+        }
+      });
+    },
+
+    /**
+     * Determine if file uploaded exceeds provided limit
+     * @access private
+     * @return {Boolean}
+     */
+    _checkSize: function() {
+      var limit = parseInt( this.$input.attr('data-limit') );
+      var size = this.$input.get(0).files[0].size / 1024 / 1024;
+      var error_obj = $('<span />', {
+        class: 'error',
+        text: this.$input.attr('data-exceeded').replace('###', limit)
+      });
+
+      if (size > limit) {
+        this.$input.after( error_obj );
+        this.$el.addClass('field_with_errors');
+
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+  };
+
+  /**
+   * A small helper library for displaying a file input field
+   * @function external:"jQuery.fn".fileinputer
+   */
+  $.fn.fileinputer = function( options ) {
+
+    var defaults = {
+      wrapper_class: 'file_input-wrapper',
+      button_class: 'file_input-button',
+      delete_class: 'file_input-delete',
+      text: 'Choose File',
+      file_text: 'No File Chosen',
+      active_class: 'js-active',
+      delete_class: 'icon-delete_x file_input-delete'
+    };
+
+    // unite the default options with the passed-in ones
+    var settings = $.extend( {}, defaults, options );
+
+    return this.each(function() {
+      var file = new FileInputer($(this), settings);
+    });
+
+  };
+
+}( jQuery ));
