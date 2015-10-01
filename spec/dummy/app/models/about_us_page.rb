7a6dbcdd1c59bdc10eb4cd438acf44f9ac07c0a1
James Kurczodyna <james@wearefine.com>
2015-09-30 23:55:37 -0700

    add tests for dynamic associations and validations

--- spec/dummy/app/models/about_us_page.rb
+++ spec/dummy/app/models/about_us_page.rb
@@ -5,7 +5,7 @@ class AboutUsPage < Fae::StaticPage
   # required to set the has_one associations, Fae::StaticPage will build these associations dynamically
   def self.fae_fields
     {
-      header: { type: Fae::TextField },
+      header: { type: Fae::TextField, validates: { presence: true } },
       introduction: { type: Fae::TextArea },
       body: { type: Fae::TextArea },
       header_image: { type: Fae::Image }
