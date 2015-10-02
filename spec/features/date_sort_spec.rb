9b3cafe808d4101d50bd028b23ae736ab7d0e128
Nichole Barrett <nichole@wearefine.com>
2015-10-02 15:08:44 -0700

    TAKE THAT TESTSSSSSSSS YOU CANT BEAT MEEEEEEEEEEE

--- spec/features/date_sort_spec.rb
+++ spec/features/date_sort_spec.rb
@@ -7,11 +7,13 @@ feature 'validations' do
     release_1 = FactoryGirl.create(:release)
     release_2 = FactoryGirl.create(:release, updated_at: 10.days.ago)
     release_3 = FactoryGirl.create(:release, updated_at: 1.day.ago)
-    @items = Release.for_fae_index
+
     visit admin_releases_path
     page.find('.sorter-mmddyy').click
 
-    expect(@items).to eq([release_2, release_3, release_1])
+    expect(page.find('tbody tr:nth-child(1)')).to have_content(release_2.name + ' ' + release_2.wine.name_en + ' ' + release_2.updated_at.strftime('%m/%d/%y') + ' Yes No Yes No')
+    expect(page.find('tbody tr:nth-child(2)')).to have_content(release_3.name + ' ' + release_3.wine.name_en + ' ' + release_3.updated_at.strftime('%m/%d/%y') + ' Yes No Yes No')
+    expect(page.find('tbody tr:nth-child(3)')).to have_content(release_1.name + ' ' + release_1.wine.name_en + ' ' + release_1.updated_at.strftime('%m/%d/%y') + ' Yes No Yes No')
   end
 
 end
