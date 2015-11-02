86d4df7bc6cd6a3ac549a390c8cf849b571a9688
Niles McGiver <niles@finedesigngroup.com>
2015-11-02 13:51:26 -0800

    add in stuff

--- spec/dummy/app/models/winemaker.rb
+++ spec/dummy/app/models/winemaker.rb
@@ -1,13 +1,16 @@
 class Winemaker < ActiveRecord::Base
   include Fae::Concerns::Models::Base
 
-  def fae_display_field
-    name
-  end
+  belongs_to :wine, touch: true
 
   acts_as_list add_new_at: :top
   default_scope { order(:position) }
 
-  belongs_to :wine, touch: true
+  has_one :winemaker_image, as: :imageable, class_name: '::Fae::Image', dependent: :destroy
+  accepts_nested_attributes_for :winemaker_image, allow_destroy: true
+
+  def fae_display_field
+    name
+  end
 
 end
