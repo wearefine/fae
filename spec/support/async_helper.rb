0b2f30b462f24d673145319bb9dd7bbbe16bf2c9
James Kurczodyna <james@wearefine.com>
2015-11-03 11:53:47 -0800

    replace sleep with eventually helper to alleviate flickering tests

---
+++ spec/support/async_helper.rb
@@ -0,0 +1,18 @@
+# eventually usage:
+# it "should return a result of 5" do
+#   eventually { long_running_thing.result.should eq(5) }
+# end
+def eventually(options = {})
+  timeout = options[:timeout] || 2
+  interval = options[:interval] || 0.1
+  time_limit = Time.now + timeout
+  loop do
+    begin
+      yield
+    rescue *[RSpec::Expectations::ExpectationNotMetError, StandardError] => error
+    end
+    return if error.nil?
+    raise error if Time.now >= time_limit
+    sleep interval
+  end
+end
