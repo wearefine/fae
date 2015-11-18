03b1b2270b309e78b9084fb2013651f466831de3
Nichole Barrett <nichole@wearefine.com>
2015-11-18 08:36:50 -0800

    specs passing

---
+++ spec/features/markdown_binding_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Rebinding Markdown' do
+
+  scenario 'should only bind new markdown elements when nested add is clicked', js: true do
+    release = FactoryGirl.create(:release, name: 'Ima Release', vintage: '2012', price: 13, varietal_id: 2, show: Date.today)
+    admin_login
+    visit edit_admin_release_path(release)
+    click_link 'Add Aroma'
+    click_link 'Add Aroma'
+
+    expect(page).to have_selector('.CodeMirror', count: 1)
+  end
+
+end
