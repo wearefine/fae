9c379f5d3873c476a531527eb0829e033c6040a4
Tim Shedor <tim@wearefine.com>
2016-02-24 15:09:15 -0800

    refs #55066 - fix breaking capybara test

--- spec/features/form_helpers/fae_nested_table_spec.rb
+++ spec/features/form_helpers/fae_nested_table_spec.rb
@@ -68,7 +68,7 @@ feature 'fae_nested_table' do
     expect(page.body).to match(/First.*Middle.*Last/)
 
     within(:css, '#winemakers_section') do
-      handle = all('tbody tr').last.find('.main_content-sortable-handle')
+      handle = all('tbody tr').last.find('.sortable-handle')
       handle.drag_to(find('thead'))
     end
 
