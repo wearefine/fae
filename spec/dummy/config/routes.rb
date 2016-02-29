6cf7f04483111a3438ae3245385d98213db0b63c
James Kurczodyna <james@wearefine.com>
2016-02-28 23:33:13 -0800

    refs #55066 - restructure legacy markup and move to it's own section

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
     resources :releases do
       post 'filter', on: :collection
     end
+    resources :legacy_releases do
+      post 'filter', on: :collection
+    end
     resources :wines
     resources :varietals
     resources :acclaims
