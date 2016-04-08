24a0648a56847dcb0fef5a437cfb3ebe29504e4d
James Kurczodyna <james@wearefine.com>
2016-04-07 23:17:45 -0700

    55178 - update generators to support new navigation

---
+++ lib/generators/fae/templates/models/concerns/navigation_concern.rb
@@ -0,0 +1,38 @@
+module Fae
+  module NavigationConcern
+    extend ActiveSupport::Concern
+
+    # define the navigation structure in this file
+    # navigation will default to the sidenav,
+    # unless you set the following in the Fae initializer:
+    #
+    #   config.has_top_nav = true
+
+    # example  structure with top nav:
+    #
+    # def structure
+    #   [
+    #     item('Top Nav Item 1', subitems: [
+    #       item('Top Nav Dropdown Item 1', class: 'custom-class', path: some_named_route_path),
+    #       item('Top Nav Dropdown Item 2', subitems: [
+    #         item('Side Nav Item 1', subitems: [
+    #           item('Side Nav Nested Item', path: some_named_route_path)
+    #         ]),
+    #         item('Side Nav Item 1', path: some_named_route_path)
+    #       ]),
+    #     ]),
+    #     item('Pages', subitems: [
+    #       item('Home', path: fae.edit_content_block_path('home')),
+    #       item('About Us', path: fae.edit_content_block_path('about_us'))
+    #     ])
+    #   ]
+    # end
+
+    def structure
+      [
+        # scaffold inject marker
+      ]
+    end
+
+  end
+end
