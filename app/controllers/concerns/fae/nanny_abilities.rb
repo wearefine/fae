6f42efea3d22520ed09d88ed28204d729f8096e0
Jason Fine <jason@finedesigngroup.com>
2016-05-23 16:15:00 -0400

    default role map should be empty

--- app/controllers/concerns/fae/nanny_abilities.rb
+++ app/controllers/concerns/fae/nanny_abilities.rb
@@ -3,10 +3,10 @@ module Fae
     extend ActiveSupport::Concern
 
     def self.access_map
-      #{
+      {
       #  'wines' => [2,3],
       #  'asset_manager/tiers' => [1,2,3]
-      #}
+      }
     end
 
   end
