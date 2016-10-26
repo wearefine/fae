059eea4a6a0c015b52357b51cad457d79f1a1190
Aaron Ransley <aaron@wearefine.com>
2016-10-26 16:05:00 -0700

    Refs #60581 - Overhauls form JS, form Hinter, as well as various form element CSS to implement a new two-column label design

--- app/assets/javascripts/fae/form/_form.js
+++ app/assets/javascripts/fae/form/_form.js
@@ -23,8 +23,40 @@ Fae.form = {
       $('.input.file').fileinputer();
     }
 
+    // Mutate DOM to support two column labels for all standard inputs
+    // @note This is done via JS because making these modifications via simple_form requires a deep dive
+    this.make_two_column_labels();
+
     // make all the hint areas
     $('.hint').hinter();
   },
 
+  make_two_column_labels: function() {
+    $('.input label').each(function() {
+      var $element = $(this);
+
+      // Reject any matches found within a checkbox
+      // if ($element.parents('.checkbox').length || $element.hasClass('checkbox')) { return; }
+
+      // Otherwise, check for presence of helper_text
+      if ($element.find('.helper_text').length) {
+
+        // If present, get all DOM nodes w/ contents(), but ignore the .helper_text
+        var label_inner = $element.contents().filter(function() {
+          return !$(this).hasClass('helper_text');
+        });
+        var helper_text = $element.find('.helper_text');
+
+        // Replace existing label w/ newly wrapped elements, sans .helper_text
+        label_inner = $('<div class="label_inner" />').html(label_inner);
+        $element.html(label_inner);
+
+        // But then add .helper_text as a sibling
+        $element.append(helper_text);
+
+        // Ensure that we mark this input as having two column label support
+        $element.addClass('label--two_col');
+      }
+    });
+  }
 };
