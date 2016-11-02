380fbc2a99acbc2dd180bb6d17f0fab696df1910
Aaron Ransley <aaron@wearefine.com>
2016-11-02 10:43:40 -0700

    Refs #60596 - Integrate feedback from Tim & James; add tests; add documentation

--- app/helpers/fae/view_helper.rb
+++ app/helpers/fae/view_helper.rb
@@ -27,13 +27,9 @@ module Fae
 
     def fae_index_image(image, path = nil)
       return if image.blank? || image.asset.blank? || image.asset.thumb.blank?
-      output = nil
-      if path
-        output = link_to path do image_tag(image.asset.thumb.url) end
-      else
-        output = image_tag(image.asset.thumb.url)
+      content_tag :div, class: 'image-mat' do
+        link_to_if path.present?, image_tag(image.asset.thumb.url), path
       end
-      "<div class='image-mat'>#{output}</div>".html_safe
     end
 
     def attr_toggle(item, column)
