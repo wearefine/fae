5c1a7842f9949aed11f79297de0956c8fcf22309
Aaron Ransley <aaron@wearefine.com>
2017-01-27 14:44:22 -0800

    Refs #60171 - Adds a handful of helpful actions to multiselects with 10 or more options

--- app/assets/javascripts/fae/form/inputs/_select.js
+++ app/assets/javascripts/fae/form/inputs/_select.js
@@ -9,6 +9,7 @@ Fae.form.select = {
 
   init: function() {
     this.multiselectOrChosen();
+    this.multiselectChosenActions();
   },
 
   /**
@@ -55,6 +56,88 @@ Fae.form.select = {
 
       }
     });
+  },
+
+  /**
+   * TODO: Create a more extensible solution allowing for various actions to be attached to a Chosen multiselect.
+   */
+  multiselectChosenActions: function() {
+    var input_select = '.input.select';
+    var $eligible_multiselects = $(input_select + ' select[multiple]:not(.multiselect)');
+    $eligible_multiselects.each(function(index, element) {
+      var $element = $(element);
+      var $options = $element.find('option');
+
+      // Ignore if select doesn't have enough options to warrant additional actions
+      if ($options.length < 10) {
+        return;
+      }
+
+      // Insert Select All btn above select
+      var $label = $element.closest(input_select).find('label:first');
+      var $actions_wrap = $('<div />', {
+        class: 'multiselect-action_wrap'
+      });
+      var $select_all_action = $('<div/>', {
+        class: 'js-multiselect-action-select_all multiselect-action',
+        html: $('<a/>', {
+          href: '#',
+          html: 'Select All'
+        })
+      });
+      var $deselect_all_action = $('<div/>', {
+        class: 'js-multiselect-action-deselect_all multiselect-action-deselect_all multiselect-action',
+        html: $('<a/>', {
+          href: '#',
+          html: 'Deselect All'
+        })
+      });
+      var $invert_selection_action = $('<div/>', {
+        class: 'js-multiselect-action-invert_selection multiselect-action-invert_selection multiselect-action',
+        html: $('<a/>', {
+          href: '#',
+          html: 'Invert Selection'
+        })
+      });
+
+      // Add actions to wraper
+      $select_all_action.appendTo($actions_wrap);
+      $deselect_all_action.appendTo($actions_wrap);
+      $invert_selection_action.appendTo($actions_wrap);
+
+      // Add wrapper below form label
+      $actions_wrap.insertAfter($label);
+    });
+
+    // Handle click on Select All btn
+    $('#js-main-content').on('click', '.js-multiselect-action-select_all', function(e) {
+      setAllOptionsSelected(e, this, true);
+    });
+
+    // Handle click on Deselect All btn
+    $('#js-main-content').on('click', '.js-multiselect-action-deselect_all', function(e) {
+      setAllOptionsSelected(e, this, false);
+    });
+
+    // Handle click on Invert Selection btn
+    $('#js-main-content').on('click', '.js-multiselect-action-invert_selection', function(e) {
+      setAllOptionsSelected(e, this, 'swap');
+    });
+
+    function setAllOptionsSelected(e, select, state) {
+      e.preventDefault();
+      var $select = $(select).closest(input_select).find('select');
+      // If requested, swap the selected state of all multiselect options
+      if (state === 'swap') {
+        $select.find('option').each(function(index, element) {
+          $(element).prop('selected', !$(element).prop('selected'));
+        });
+      } else {
+        // Otherwise set selection state per provided state boolean
+        $select.find('option').prop('selected', state);
+      }
+      $select.trigger('chosen:updated');
+    }
   }
 
 };
