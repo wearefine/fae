15f95143e105e5f13dae2f2fd2b42ae2172eb0a8
Aaron Ransley <aaron@wearefine.com>
2017-03-15 12:49:54 -0700

    Refs #60171 - Heavy mods to Select All functionality on Chosen multiselects

--- app/assets/javascripts/fae/form/inputs/_select.js
+++ app/assets/javascripts/fae/form/inputs/_select.js
@@ -59,83 +59,86 @@ Fae.form.select = {
   },
 
   /**
-   * TODO: Create a more extensible solution allowing for various actions to be attached to a Chosen multiselect.
+   * Adds functionality for Select All and Deselect All multiselect options
    */
   multiselectChosenActions: function() {
-    var input_select = '.input.select';
-    var $eligible_multiselects = $(input_select + ' select[multiple]:not(.multiselect)');
+    var query_input_select = '.input.select';
+    var query_eligible_multiselects = query_input_select + ' select[multiple]:not(.multiselect)';
+    var select_all_value = 'multiselect_action-select_all';
+
+    var $eligible_multiselects = $(query_eligible_multiselects);
     $eligible_multiselects.each(function(index, element) {
       var $element = $(element);
       var $options = $element.find('option');
 
       // Ignore if select doesn't have enough options to warrant additional actions
-      if ($options.length < 10) {
+      if ($options.length < 2) {
         return;
       }
 
       // Insert Select All btn above select
-      var $label = $element.closest(input_select).find('label:first');
-      var $actions_wrap = $('<div />', {
-        class: 'multiselect-action_wrap'
-      });
-      var $select_all_action = $('<div/>', {
-        class: 'js-multiselect-action-select_all multiselect-action',
-        html: $('<a/>', {
-          href: '#',
-          html: 'Select All'
-        })
-      });
+      var $label = $element.closest(query_input_select).find('label:first');
       var $deselect_all_action = $('<div/>', {
         class: 'js-multiselect-action-deselect_all multiselect-action-deselect_all multiselect-action',
         html: $('<a/>', {
           href: '#',
-          html: 'Deselect All'
-        })
-      });
-      var $invert_selection_action = $('<div/>', {
-        class: 'js-multiselect-action-invert_selection multiselect-action-invert_selection multiselect-action',
-        html: $('<a/>', {
-          href: '#',
-          html: 'Invert Selection'
+          tabindex: '-1',
+          html: 'Deselect&nbsp;All'
         })
       });
 
       // Add actions to wraper
-      $select_all_action.appendTo($actions_wrap);
-      $deselect_all_action.appendTo($actions_wrap);
-      $invert_selection_action.appendTo($actions_wrap);
+      $deselect_all_action.appendTo($label);
 
-      // Add wrapper below form label
-      $actions_wrap.insertAfter($label);
+      // Add special "Select All" option and notify Chosen of new option
+      addSelectAllOption($element)
+
+      // Mark label wrapper as having multiselect actions for styling
+      $label.addClass('has-multiselect-actions');
     });
 
-    // Handle click on Select All btn
-    $('#js-main-content').on('click', '.js-multiselect-action-select_all', function(e) {
-      setAllOptionsSelected(e, this, true);
+    // Watch select for changes and add, remove, or carry out Select All option as needed
+    $('#js-main-content').on('change', query_eligible_multiselects, function(e) {
+      var $element = $(e.currentTarget);
+
+      // Fizzle on empty values
+      if (!$element.val()) { return; }
+
+      // Intercept selections of the Select All option and select all other options.
+      var requesting_select_all = $element.val().indexOf(select_all_value) != -1;
+      if (requesting_select_all) {
+        setAllOptionsSelected($element, true);
+      }
     });
 
     // Handle click on Deselect All btn
     $('#js-main-content').on('click', '.js-multiselect-action-deselect_all', function(e) {
-      setAllOptionsSelected(e, this, false);
-    });
-
-    // Handle click on Invert Selection btn
-    $('#js-main-content').on('click', '.js-multiselect-action-invert_selection', function(e) {
-      setAllOptionsSelected(e, this, 'swap');
-    });
-
-    function setAllOptionsSelected(e, select, state) {
       e.preventDefault();
-      var $select = $(select).closest(input_select).find('select');
-      // If requested, swap the selected state of all multiselect options
-      if (state === 'swap') {
-        $select.find('option').each(function(index, element) {
-          $(element).prop('selected', !$(element).prop('selected'));
-        });
-      } else {
-        // Otherwise set selection state per provided state boolean
-        $select.find('option').prop('selected', state);
-      }
+      setAllOptionsSelected(this, false);
+    });
+
+    // Add special "Select All" option
+    function addSelectAllOption($element) {
+      var $select_all_option = $('<option />', {
+        value: select_all_value,
+        text: 'Select All'
+      });
+      $select_all_option.prependTo($element);
+      $element.trigger('chosen:updated');
+    }
+
+    // Remove "Select All" option
+    function ignoreSelectAllOption($element) {
+      $element.find('option[value="' + select_all_value + '"]').prop('selected', false);
+    }
+
+    // Mark all options as selected or not
+    function setAllOptionsSelected(select, state) {
+      var $select = $(select).closest(query_input_select).find('select');
+      // Set selection state per provided state boolean
+      $select.find('option').prop('selected', state);
+      // Always ignore magic Select All btn
+      $select.find('option[value="' + select_all_value + '"]').prop('selected', false);
       $select.trigger('chosen:updated');
     }
   }
