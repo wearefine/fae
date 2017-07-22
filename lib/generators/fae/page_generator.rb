94816d7ba2942468aa2e7ff3486df97332976feb
Hans Lemuet <hans@etaminstudio.com>
2017-07-22 11:15:51 +0200

    Added :: before File in generators

--- lib/generators/fae/page_generator.rb
+++ lib/generators/fae/page_generator.rb
@@ -3,7 +3,7 @@ require_relative 'base_generator'
 module Fae
   class PageGenerator < Fae::BaseGenerator
     desc 'Creates model with dependancies for Fae Static Pages'
-    source_root File.expand_path('../templates', __FILE__)
+    source_root ::File.expand_path('../templates', __FILE__)
 
     @@attributes = {}
 
