f084a2ba2792a0013ab509f3f61ce682175b8080
Jason Fine <jason@finedesigngroup.com>
2017-08-03 09:54:25 -0400

    initial pass for asset cloning

---
+++ spec/requests/cloning_assets_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'cloning assets' do
+
+  it 'should clone fae assets' do
+
+    release = FactoryGirl.create(:release, name: 'Ima Release')
+    bottle_shot = FactoryGirl.create(:fae_image, imageable_type: 'Release', imageable_id: release.id, attached_as: 'bottle_shot')
+    label_pdf = FactoryGirl.create(:fae_file, fileable_type: 'Release', fileable_id: release.id, attached_as: 'label_pdf')
+
+    admin_login
+    post admin_releases_path(from_existing: 1)
+
+    cloned_release = Release.find_by_name('Ima Release-2')
+
+    expect(cloned_release.bottle_shot).to_not eq(bottle_shot)
+    expect(cloned_release.bottle_shot.asset.file.filename).to eq(bottle_shot.asset.file.filename)
+
+    expect(cloned_release.label_pdf).to_not eq(label_pdf)
+    expect(cloned_release.label_pdf.asset.file.filename).to eq(label_pdf.asset.file.filename)
+
+  end
+
+end
