50885949f14caed755467240d620ac0460773b15
Jeremy Clemans <jeremy@wearefine.com>
2019-01-04 09:24:32 -0800

    if there is a new highlight color, write it to a highlight_color css file, recompile the app assets, and restart the server.

--- app/controllers/fae/options_controller.rb
+++ app/controllers/fae/options_controller.rb
@@ -12,6 +12,13 @@ module Fae
     # PATCH/PUT /options/1
     def update
       if @option.update(option_params)
+        # add custom css and recompile assets to apply the custom highlight color if changed
+        if option_params[:highlight_color].present?
+          css_text = "$c-custom-highlight: #{option_params[:highlight_color]};"
+          file = File.open('app/assets/stylesheets/highlight_color.scss', 'w') { |f| f << css_text; f.close }
+          system 'rake assets:precompile RAILS_ENV=production'
+          system 'touch tmp/restart.txt'
+        end
         flash[:notice] = 'Option was successfully updated.'
         redirect_to :action => :edit
       else
@@ -21,9 +28,9 @@ module Fae
 
     private
 
-      # Only allow a trusted parameter "white list" through.
-      def option_params
-        params.require(:option).permit(:title, :highlight_color, :time_zone, :colorway, :stage_url, :live_url, logo_attributes: [:id, :asset, :asset_cache, :attached_as, :alt], favicon_attributes: [:id, :asset, :asset_cache, :attached_as, :alt])
-      end
+    # Only allow a trusted parameter "white list" through.
+    def option_params
+      params.require(:option).permit(:title, :highlight_color, :time_zone, :colorway, :stage_url, :live_url, logo_attributes: [:id, :asset, :asset_cache, :attached_as, :alt], favicon_attributes: [:id, :asset, :asset_cache, :attached_as, :alt])
+    end
   end
 end
