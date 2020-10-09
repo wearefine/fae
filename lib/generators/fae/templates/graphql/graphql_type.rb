7106cddb550656e5c5de8fb688b9d302245d9f11
Jeremy Clemans <jeremy@wearefine.com>
2020-10-08 17:14:19 -0700

    adds a conditional check in the graphql type generator so that a field does not get added for an associated Fae::StaticPage type, since that is never exposed to the front end.

--- lib/generators/fae/templates/graphql/graphql_type.rb
+++ lib/generators/fae/templates/graphql/graphql_type.rb
@@ -5,7 +5,9 @@ class Types::<%= class_name %>Type < Types::BaseObject
   field :id, ID, null: false
 <% if @graphql_attributes.present? -%>
 <% @graphql_attributes.each do |graphql_object| -%>
+<% if graphql_object[:attr] != :static_page -%>
   field :<%= graphql_object[:attr] %>, <%= graphql_object[:type] %>, null: true
+<% end -%>
 <% end -%>
 <% end -%>
   field :created_at, String, null: false
