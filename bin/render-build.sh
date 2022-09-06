ce3eedc92d39a108aa1bd4546dae976732aebe2d
Jason Fine <jason@wearefine.com>
2022-09-06 14:55:14 -0400

    deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,4 +8,4 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 echo 'hi there'
-bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
