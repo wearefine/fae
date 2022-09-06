e1a24ad08af38cf84d72af951af8d94653e471b4
Jason Fine <jason@wearefine.com>
2022-09-06 11:15:40 -0400

    deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 ls -la
-# cd /spec/dummy
-# bundle install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
-# bundle exec rake db:migrate
+cd spec/dummy
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
