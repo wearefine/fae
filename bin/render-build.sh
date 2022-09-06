c148c3f679e38405248e977373c81201a0d05987
Jason Fine <jason@wearefine.com>
2022-09-06 15:35:33 -0400

    deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,6 +8,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 echo 'try this'
-bundle exec rake db:schema:load DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rake db:migrate
 echo 'after'
