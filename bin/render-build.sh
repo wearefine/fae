0519e1f5244fcaf8ddce361e7224283059b92bd3
Jason Fine <jason@wearefine.com>
2022-09-06 15:02:50 -0400

    deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,9 @@ set -o errexit
 ls -la
 cd spec/dummy
 bundle install
+echo 'try this'
+ENV['DATABASE_URL']
+bundle exec bin/rails db:migrate RAILS_ENV=production
+echo 'after'
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-echo 'hi there'
-bundle exec bin/rails db:migrate RAILS_ENV=production
