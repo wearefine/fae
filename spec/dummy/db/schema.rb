bbe75e6da0c8af6dbe45970664f3045f8d71d080
JJ <jjordan@wearefine.com>
2022-11-01 15:53:50 -0400

    FAE MFA - updated DB, devise 2fa needs postgres

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
-  create_table "acclaims", id: :integer, charset: "utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "acclaims", id: :serial, force: :cascade do |t|
     t.string "score"
     t.string "publication"
     t.text "description"
@@ -24,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.date "publication_date"
   end
 
-  create_table "aromas", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "aromas", id: :serial, force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "position"
@@ -36,14 +39,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["release_id"], name: "index_aromas_on_release_id"
   end
 
-  create_table "article_categories", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "article_categories", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "position"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "articles", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "articles", id: :serial, force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "position"
@@ -53,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["article_category_id"], name: "index_articles_on_article_category_id"
   end
 
-  create_table "beers", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "beers", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "seo_title"
     t.string "seo_description"
@@ -63,7 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "cats", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "cats", id: :serial, force: :cascade do |t|
     t.string "name"
     t.boolean "friendly"
     t.text "description"
@@ -72,7 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.integer "aroma_id"
   end
 
-  create_table "coaches", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "coaches", id: :serial, force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "role"
@@ -83,7 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["team_id"], name: "index_coaches_on_team_id"
   end
 
-  create_table "event_releases", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "event_releases", id: :serial, force: :cascade do |t|
     t.integer "release_id"
     t.integer "event_id"
     t.datetime "created_at", precision: nil
@@ -92,7 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["release_id"], name: "index_event_releases_on_release_id"
   end
 
-  create_table "events", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "events", id: :serial, force: :cascade do |t|
     t.string "name"
     t.date "start_date"
     t.date "end_date"
@@ -104,7 +107,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.integer "position"
   end
 
-  create_table "fae_changes", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_changes", id: :serial, force: :cascade do |t|
     t.integer "changeable_id"
     t.string "changeable_type"
     t.integer "user_id"
@@ -117,7 +120,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["user_id"], name: "index_fae_changes_on_user_id"
   end
 
-  create_table "fae_deploy_hooks", charset: "utf8", force: :cascade do |t|
+  create_table "fae_deploy_hooks", force: :cascade do |t|
     t.string "url"
     t.string "environment"
     t.datetime "created_at", precision: nil, null: false
@@ -127,7 +130,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["position"], name: "index_fae_deploy_hooks_on_position"
   end
 
-  create_table "fae_files", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_files", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "asset"
     t.string "fileable_type"
@@ -144,7 +147,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["fileable_type", "fileable_id"], name: "index_fae_files_on_fileable"
   end
 
-  create_table "fae_form_managers", charset: "utf8", force: :cascade do |t|
+  create_table "fae_form_managers", force: :cascade do |t|
     t.string "form_manager_model_name"
     t.integer "form_manager_model_id"
     t.text "fields"
@@ -154,7 +157,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["form_manager_model_name"], name: "index_fae_form_managers_on_form_manager_model_name"
   end
 
-  create_table "fae_images", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_images", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "asset"
     t.string "imageable_type"
@@ -173,7 +176,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["imageable_type", "imageable_id"], name: "index_fae_images_on_imageable"
   end
 
-  create_table "fae_options", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_options", id: :serial, force: :cascade do |t|
     t.string "title"
     t.string "time_zone"
     t.string "colorway"
@@ -185,14 +188,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["singleton_guard"], name: "index_fae_options_on_singleton_guard", unique: true
   end
 
-  create_table "fae_roles", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_roles", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "position"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
 
-  create_table "fae_static_pages", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_static_pages", id: :serial, force: :cascade do |t|
     t.string "title"
     t.integer "position", default: 0
     t.boolean "on_stage", default: true
@@ -203,7 +206,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["slug"], name: "index_fae_static_pages_on_slug"
   end
 
-  create_table "fae_text_areas", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_text_areas", id: :serial, force: :cascade do |t|
     t.string "label"
     t.text "content"
     t.integer "position", default: 0
@@ -222,7 +225,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["position"], name: "index_fae_text_areas_on_position"
   end
 
-  create_table "fae_text_fields", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_text_fields", id: :serial, force: :cascade do |t|
     t.string "contentable_type"
     t.integer "contentable_id"
     t.string "attached_as"
@@ -240,7 +243,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["position"], name: "index_fae_text_fields_on_position"
   end
 
-  create_table "fae_users", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "fae_users", id: :serial, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -268,7 +271,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.string "otp_secret"
     t.integer "consumed_timestep"
     t.boolean "otp_required_for_login"
-    t.text "otp_backup_codes"
+    t.text "otp_backup_codes", array: true
     t.index ["confirmation_token"], name: "index_fae_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_fae_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_fae_users_on_reset_password_token", unique: true
@@ -276,14 +279,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["unlock_token"], name: "index_fae_users_on_unlock_token", unique: true
   end
 
-  create_table "jerseys", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "jerseys", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "color"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "locations", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "locations", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "contact_id"
     t.datetime "created_at", precision: nil
@@ -291,14 +294,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["contact_id"], name: "index_locations_on_contact_id"
   end
 
-  create_table "milestones", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "milestones", id: :serial, force: :cascade do |t|
     t.integer "year"
     t.string "description"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "people", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "people", id: :serial, force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
@@ -308,7 +311,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.integer "position"
   end
 
-  create_table "players", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "players", id: :serial, force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "number"
@@ -319,7 +322,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
-  create_table "release_notes", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "release_notes", id: :serial, force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "position"
@@ -329,7 +332,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["release_id"], name: "index_release_notes_on_release_id"
   end
 
-  create_table "release_selling_points", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "release_selling_points", id: :serial, force: :cascade do |t|
     t.integer "release_id"
     t.integer "selling_point_id"
     t.integer "position"
@@ -337,7 +340,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.datetime "updated_at", precision: nil
   end
 
-  create_table "releases", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "releases", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "slug"
     t.text "intro"
@@ -366,7 +369,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.boolean "is_something", default: false
   end
 
-  create_table "selling_points", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "selling_points", id: :serial, force: :cascade do |t|
     t.string "name"
     t.boolean "on_stage", default: true
     t.boolean "on_prod", default: false
@@ -375,7 +378,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.datetime "updated_at", precision: nil
   end
 
-  create_table "teams", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "teams", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "city"
     t.text "history"
@@ -383,7 +386,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.datetime "updated_at", precision: nil
   end
 
-  create_table "validation_testers", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "validation_testers", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "slug"
     t.string "second_slug"
@@ -403,7 +406,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.string "second_youtube_url"
   end
 
-  create_table "varietals", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "varietals", id: :serial, force: :cascade do |t|
     t.string "name"
     t.boolean "on_stage", default: true
     t.boolean "on_prod", default: false
@@ -412,7 +415,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.datetime "updated_at", precision: nil
   end
 
-  create_table "winemakers", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "winemakers", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "position"
     t.integer "wine_id"
@@ -422,7 +425,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_165831) do
     t.index ["wine_id"], name: "index_winemakers_on_wine_id"
   end
 
-  create_table "wines", id: :integer, charset: "utf8", force: :cascade do |t|
+  create_table "wines", id: :serial, force: :cascade do |t|
     t.string "name_en"
     t.boolean "on_stage", default: true
     t.boolean "on_prod", default: false
