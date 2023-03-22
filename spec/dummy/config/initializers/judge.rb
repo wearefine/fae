52f5ad1cc77b0647abe815839b2fa610e7ef9bb1
Jay <jasoncruze@gmail.com>
2023-03-22 09:04:38 -0700

    updates judge config to expose new association syntax for validations, this also fixes a failing test

--- spec/dummy/config/initializers/judge.rb
+++ spec/dummy/config/initializers/judge.rb
@@ -1,6 +1,8 @@
 Rails.application.config.to_prepare do
-  Judge.configure do
+  Judge.configure do |config|
     expose Release, :name
     expose ValidationTester, :slug, :second_slug
+
+    config.use_association_name_for_validations true
   end
 end
