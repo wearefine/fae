684d1f878ed7a2a2ba82c064dc0a7901320cf07d
JJ <jjordan@wearefine.com>
2023-11-16 10:00:07 -0500

    testing no translate

--- app/helpers/fae/form_helper.rb
+++ app/helpers/fae/form_helper.rb
@@ -143,7 +143,7 @@ module Fae
     end
 
     def translate_button(f, attribute, options)
-      if Fae::Option.instance.translate_language && !attribute.to_s.end_with?('_en') && Fae.languages.keys.any? { |lang| attribute.to_s.include?(lang.to_s) }
+      if Fae::Option.instance.translate_language && !attribute.to_s.end_with?('_en') && Fae.languages.keys.any? { |lang| attribute.to_s.include?(lang.to_s) } && options[:translate] != false
         translate = content_tag(:span, 'Translate', class: 'button js-translate-button', style: 'position: absolute; margin-left: 10px;')
       end
       options[:translate] = translate.html_safe if translate.present?
