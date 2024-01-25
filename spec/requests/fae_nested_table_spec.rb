720251ab54b031bd8d3424c0aec4a4d8cb304c48
JJ <jjordan@wearefine.com>
2024-01-25 13:54:26 -0500

    Tests passing, with url method overrides

--- spec/requests/fae_nested_table_spec.rb
+++ spec/requests/fae_nested_table_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 describe 'custom columns' do
+
+  before do
+    def edit_admin_release_path(release)
+      Rails.application.routes.url_helpers.edit_admin_release_path(release)
+    end
+  end
+
   it 'should get correct title and attr' do
     release = FactoryBot.create(:release)
     admin_login
--- spec/requests/selling_points_spec.rb
+++ spec/requests/selling_points_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe 'selling_points#index' do
 
+  before do
+    def admin_selling_points_path
+      Rails.application.routes.url_helpers.admin_selling_points_path
+    end
+  end
+
   it 'should return found' do
     admin_login
     get admin_selling_points_path
@@ -13,6 +19,12 @@ end
 
 describe 'selling_points#new' do
 
+  before do
+    def new_admin_selling_point_path
+      Rails.application.routes.url_helpers.new_admin_selling_point_path
+    end
+  end
+
   it 'should return found' do
     admin_login
     get new_admin_selling_point_path
@@ -23,6 +35,12 @@ describe 'selling_points#new' do
 end
 
 describe 'selling_points#edit' do
+
+  before do
+    def edit_admin_selling_point_path(selling_point)
+      Rails.application.routes.url_helpers.edit_admin_selling_point_path(selling_point)
+    end
+  end
 
   it 'should return found' do
     selling_point = FactoryBot.create(:selling_point)
