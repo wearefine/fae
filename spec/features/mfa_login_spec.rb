822615412595a9b43d1b927ce1771a3f2f67267a
JJ <jjordan@wearefine.com>
2024-01-29 15:18:15 -0500

    test comments

--- spec/features/mfa_login_spec.rb
+++ spec/features/mfa_login_spec.rb
@@ -5,25 +5,24 @@ feature 'User with OTP two factor enabled' do
 
   before :each do
     create_super_user
+
+    # Tun on MFA
     option = Fae::Option.instance
     option.site_mfa_enabled = true
     option.save
   end
 
-  scenario 'cannot login without unknown user', js: true do
-    # When I enter a username and password that does not exist
+  scenario 'cannot login without unknown user' do
     visit fae.new_user_session_path
     fill_in 'user_email', with: 'someone@example.com'
     fill_in 'user_password', with: 'letmein'
     click_button 'Submit'
 
-    # Then I expect to see an error message
     expect(page).to have_content("You’ll need a user name, password and MFA code that works.")
     expect(page).to_not have_content('Welcome to Fae')
   end
 
   scenario 'cannot login without a valid OTP' do
-    # Given I am a user that has OTP two factor authentication enabled
     user = FactoryBot.create(:fae_user, :with_otp,
       email: 'test@test.com',
       password: 'passord1',
@@ -31,20 +30,17 @@ feature 'User with OTP two factor enabled' do
       active: true
       )
 
-    # And I enter my username and password
     visit fae.new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: 'passord1'
     fill_in 'user_otp_attempt', with: 'invalid-otp'
     click_button 'Submit'
 
-    # I expect to see an error message
     expect(page).to have_content("You’ll need a user name, password and MFA code that works.")
     expect(page).to_not have_content('Welcome to Fae')
   end
 
   scenario 'can login when providing a valid OTP' do
-    # Given I am a user that has OTP two factor authentication enabled
     user = FactoryBot.create(:fae_user, :with_otp,
       email: 'test@test.com',
       password: 'passord1',
@@ -52,14 +48,12 @@ feature 'User with OTP two factor enabled' do
       active: true
       )
 
-    # And I enter my username and password
     visit fae.new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: 'passord1'
     fill_in 'user_otp_attempt', with: user.current_otp
     click_button 'Submit'
 
-    # I expect to be successfully logged in and taken to the dashboard
     expect(current_path).to eq(fae.root_path)
     expect(page).to have_content("Welcome to Fae")
   end
