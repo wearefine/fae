abb6a81bf8c267a58a515428b8c65b0306bcbf80
JJ <jjordan@wearefine.com>
2024-01-30 11:45:29 -0500

    simple fix for devise two factor counting each bad login as two bad logins

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -175,7 +175,10 @@ Devise.setup do |config|
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  config.maximum_attempts = 5
+  # Note, this was increased to 10 because devise two factor checks it twice, so every failed
+  # login attemp counts as two failed attemps.
+  # Issue https://github.com/devise-two-factor/devise-two-factor/issues/127
+  config.maximum_attempts = 10
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   config.unlock_in = 1.minute
