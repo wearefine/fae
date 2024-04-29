1f47fd2fdd5c685ae0afffc0144c214d0c264ac8
Jason Fine <jason@wearefine.com>
2024-04-29 12:46:29 -0400

    restore test

--- spec/helpers/fae_application_helper_spec.rb
+++ spec/helpers/fae_application_helper_spec.rb
@@ -9,15 +9,15 @@ describe Fae::ApplicationHelper do
       @winemaker = FactoryBot.create(:winemaker, winemaker_image: @winemaker_image)
     end
 
-    # it 'should display an image tag for an image field' do
-    #   expect(col_name_or_image(@winemaker, :winemaker_image)).to include("<img")
-    #   expect(col_name_or_image(@winemaker, :winemaker_image)).to include("/system/uploads/fae/image/asset/#{@winemaker_image.id}/thumb_test.jpg")
-    # end
+    it 'should display an image tag for an image field' do
+      expect(col_name_or_image(@winemaker, :winemaker_image)).to include("<img")
+      expect(col_name_or_image(@winemaker, :winemaker_image)).to include("/system/uploads/fae/image/asset/#{@winemaker_image.id}/thumb_test.jpg")
+    end
 
-    # it 'should display an image tag for an image field from a custom method' do
-    #   expect(col_name_or_image(@winemaker, :winemaker_image)).to include("<img")
-    #   expect(col_name_or_image(@winemaker, :winemaker_image)).to include("/system/uploads/fae/image/asset/#{@winemaker_image.id}/thumb_test.jpg")
-    # end
+    it 'should display an image tag for an image field from a custom method' do
+      expect(col_name_or_image(@winemaker, :winemaker_image)).to include("<img")
+      expect(col_name_or_image(@winemaker, :winemaker_image)).to include("/system/uploads/fae/image/asset/#{@winemaker_image.id}/thumb_test.jpg")
+    end
 
     it 'should display original image when filetype is svg' do
       winemaker_svg = FactoryBot.build(:fae_image_svg, imageable_type: 'Winemaker', imageable_id: 1, attached_as: 'winemaker_image')
@@ -28,33 +28,33 @@ describe Fae::ApplicationHelper do
     end
   end
 
-  # describe "#change_item_link" do
+  describe "#change_item_link" do
 
-  #   it 'should display a link with data from the fae_dispaly_field if its a string or an integer' do
-  #     @user = FactoryBot.build_stubbed(:fae_user)
-  #     @item = FactoryBot.build_stubbed(:milestone, year: 2016)
-  #     @change = FactoryBot.build_stubbed(:fae_change, changeable: @item, user: @user )
+    it 'should display a link with data from the fae_dispaly_field if its a string or an integer' do
+      @user = FactoryBot.build_stubbed(:fae_user)
+      @item = FactoryBot.build_stubbed(:milestone, year: 2016)
+      @change = FactoryBot.build_stubbed(:fae_change, changeable: @item, user: @user )
 
-  #     @item2 = FactoryBot.build_stubbed(:varietal, name: "Example Name")
-  #     @change2 = FactoryBot.build_stubbed(:fae_change, changeable: @item2, user: @user )
+      @item2 = FactoryBot.build_stubbed(:varietal, name: "Example Name")
+      @change2 = FactoryBot.build_stubbed(:fae_change, changeable: @item2, user: @user )
 
-  #     expect(change_item_link(@change)).to include("Milestone: 2016")
-  #     expect(change_item_link(@change2)).to include("Varietal: Example Name")
+      expect(change_item_link(@change)).to include("Milestone: 2016")
+      expect(change_item_link(@change2)).to include("Varietal: Example Name")
 
-  #   end
+    end
 
-  #   it 'should display the changeable_id for the item field if changeable items fae_dispaly_field is nil' do
+    it 'should display the changeable_id for the item field if changeable items fae_dispaly_field is nil' do
 
-  #     @user = FactoryBot.build_stubbed(:fae_user)
-  #     @item = FactoryBot.build_stubbed(:varietal, name: nil)
-  #     @change = FactoryBot.build_stubbed(:fae_change, changeable: @item, user: @user )
-  #     @item2 = FactoryBot.build_stubbed(:milestone, year: nil)
-  #     @change2 = FactoryBot.build_stubbed(:fae_change, changeable: @item2, user: @user )
+      @user = FactoryBot.build_stubbed(:fae_user)
+      @item = FactoryBot.build_stubbed(:varietal, name: nil)
+      @change = FactoryBot.build_stubbed(:fae_change, changeable: @item, user: @user )
+      @item2 = FactoryBot.build_stubbed(:milestone, year: nil)
+      @change2 = FactoryBot.build_stubbed(:fae_change, changeable: @item2, user: @user )
 
-  #     expect(change_item_link(@change)).to include("Varietal: ##{@item.id}")
-  #     expect(change_item_link(@change2)).to include("Milestone: ##{@item2.id}")
+      expect(change_item_link(@change)).to include("Varietal: ##{@item.id}")
+      expect(change_item_link(@change2)).to include("Milestone: ##{@item2.id}")
 
-  #   end
-  # end
+    end
+  end
 
 end
