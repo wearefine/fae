15650cdb3152fa07d67f834ccb2d02a3e847bcb6
Jason Fine <jason@wearefine.com>
2024-06-05 11:31:31 -0400

    documentation

--- spec/dummy/app/models/hero_component.rb
+++ spec/dummy/app/models/hero_component.rb
@@ -1,13 +1,12 @@
 class HeroComponent < ApplicationRecord
   include Fae::BaseModelConcern
+  include Fae::BaseFlexComponentConcern
+  has_flex_component name
+  
   has_fae_image :image
 
   def fae_display_field
     title
   end
-
-  include Fae::BaseFlexComponentConcern
-
-  has_flex_component name
 
 end
