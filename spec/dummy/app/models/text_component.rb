2d7dda4ca4497421f20775e28955d7f59cbd18ee
Jason Fine <jason@wearefine.com>
2024-06-05 11:31:31 -0400

    documentation

--- spec/dummy/app/models/text_component.rb
+++ spec/dummy/app/models/text_component.rb
@@ -1,10 +1,11 @@
 class TextComponent < ApplicationRecord
   include Fae::BaseModelConcern
+  include Fae::BaseFlexComponentConcern
+  has_flex_component name
+  
   def fae_display_field
     name
   end
-  include Fae::BaseFlexComponentConcern
-  has_flex_component name
 
 
 end
