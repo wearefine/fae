5274926fe7cdf7a0659cc4baf8ea5696e7a52a16
Jason Fine <jason@wearefine.com>
2024-07-03 11:11:22 -0400

    rm commented code

--- spec/dummy/app/models/about_us_page.rb
+++ spec/dummy/app/models/about_us_page.rb
@@ -3,7 +3,6 @@ class AboutUsPage < Fae::StaticPage
   @slug = 'about_us'
 
   fae_translate :introduction, :body
-  # fae_image_translate :header_image
 
   # required to set the has_one associations, Fae::StaticPage will build these associations dynamically
   def self.fae_fields
