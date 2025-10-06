43b737d80ef84fd734462a9c00a12bec2f1e169c
Jason Fine <jason@wearefine.com>
2025-10-06 10:41:20 -0400

    rebase to master, migrate

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -157,6 +157,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_01_145909) do
     t.index ["fileable_type", "fileable_id"], name: "index_fae_files_on_fileable"
   end
 
+  create_table "fae_flex_components", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
+    t.string "flex_componentable_type", null: false
+    t.bigint "flex_componentable_id", null: false
+    t.string "component_model"
+    t.integer "component_id"
+    t.integer "position"
+    t.boolean "on_stage", default: true
+    t.boolean "on_prod", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["component_id"], name: "index_fae_flex_components_on_component_id"
+    t.index ["component_model"], name: "index_fae_flex_components_on_component_model"
+    t.index ["flex_componentable_type", "flex_componentable_id"], name: "index_fae_flex_components_on_flex_componentable"
+    t.index ["on_prod"], name: "index_fae_flex_components_on_on_prod"
+    t.index ["on_stage"], name: "index_fae_flex_components_on_on_stage"
+    t.index ["position"], name: "index_fae_flex_components_on_position"
+  end
+
   create_table "fae_form_managers", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
     t.string "form_manager_model_name"
     t.integer "form_manager_model_id"
@@ -335,24 +353,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_01_145909) do
     t.check_constraint "json_valid(`otp_backup_codes`)", name: "otp_backup_codes"
   end
 
-  create_table "flex_components", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
-    t.string "flex_componentable_type", null: false
-    t.bigint "flex_componentable_id", null: false
-    t.string "component_model"
-    t.integer "component_id"
-    t.integer "position"
-    t.boolean "on_stage", default: true
-    t.boolean "on_prod", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["component_id"], name: "index_flex_components_on_component_id"
-    t.index ["component_model"], name: "index_flex_components_on_component_model"
-    t.index ["flex_componentable_type", "flex_componentable_id"], name: "index_flex_components_on_flex_componentable"
-    t.index ["on_prod"], name: "index_flex_components_on_on_prod"
-    t.index ["on_stage"], name: "index_flex_components_on_on_stage"
-    t.index ["position"], name: "index_flex_components_on_position"
-  end
-
   create_table "hero_components", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -404,7 +404,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_01_145909) do
 
   create_table "poly_things", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
     t.string "name_en"
-    t.string "name"
     t.string "poly_thingable_type"
     t.bigint "poly_thingable_id"
     t.datetime "created_at", precision: nil, null: false
