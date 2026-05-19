d92047f37d835ec9492fda8e18a1dcefcf96f4d8
Jason Fine <jason@wearefine.com>
2026-05-19 13:53:26 -0400

    fae:join_model supports modulized class names

--- lib/generators/fae/join_model_generator.rb
+++ lib/generators/fae/join_model_generator.rb
@@ -2,10 +2,14 @@ module Fae
   class JoinModelGenerator < Rails::Generators::Base
     source_root ::File.expand_path('../templates', __FILE__)
 
+    PASCAL_CASE_MODEL_NAME = /\A[A-Z][A-Za-z0-9]*(?:::[A-Z][A-Za-z0-9]*)*\z/
+
     argument :owner_model, type: :string
     argument :joined_model, type: :string
 
     def go
+      validate_model_name!(owner_model_name, 'owner_model')
+      validate_model_name!(joined_model_name, 'joined_model')
       generate_join_model
       generate_join_model_file
     end
@@ -37,15 +41,44 @@ module Fae
     end
 
     def owner_association_name
-      normalize_model_name(owner_model)
+      demodulized_model_name(owner_model_name)
     end
 
     def joined_association_name
-      normalize_model_name(joined_model)
+      demodulized_model_name(joined_model_name)
     end
 
-    def normalize_model_name(model_name)
-      model_name.to_s.strip.split('::').last.underscore
+    def owner_belongs_to_class_name_option
+      belongs_to_class_name_option(owner_model_name)
+    end
+
+    def joined_belongs_to_class_name_option
+      belongs_to_class_name_option(joined_model_name)
+    end
+
+    def owner_model_name
+      owner_model.to_s.strip
+    end
+
+    def joined_model_name
+      joined_model.to_s.strip
+    end
+
+    def demodulized_model_name(model_name)
+      model_name.split('::').last.underscore
+    end
+
+    def belongs_to_class_name_option(model_name)
+      return '' unless model_name.include?('::')
+
+      ", class_name: '#{model_name}'"
+    end
+
+    def validate_model_name!(model_name, argument_name)
+      return if model_name.match?(PASCAL_CASE_MODEL_NAME)
+
+      raise Thor::Error,
+            "#{argument_name} must be PascalCase (optionally namespaced), e.g. StaticPage or Fae::StaticPage"
     end
   end
 end
--- lib/generators/fae/templates/models/join_model.rb
+++ lib/generators/fae/templates/models/join_model.rb
@@ -1,10 +1,10 @@
 class <%= join_model_class_name %> < ApplicationRecord
 
+  belongs_to :<%= owner_association_name %>, touch: true<%= owner_belongs_to_class_name_option %>
+  belongs_to :<%= joined_association_name %><%= joined_belongs_to_class_name_option %>
+
   acts_as_list add_new_at: :top, scope: :<%= owner_association_name %>
   default_scope { order(:position) }
-
-  belongs_to :<%= owner_association_name %>, touch: true
-  belongs_to :<%= joined_association_name %>
 
   def fae_display_field
     <%= joined_association_name %>&.fae_display_field
